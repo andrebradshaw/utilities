0170012a9d27a7ffcb2542f0c4f54747af4d7e35
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-12-30 00:34:07 -0500

    Update convertTable2Array.js

--- convertTable2Array.js
+++ convertTable2Array.js
@@ -1,40 +1,15 @@
-function convertTable2Array() {
-    sel = window.getSelection();
-    if (sel.rangeCount && sel.getRangeAt) {
-        range = sel.getRangeAt(0);
-    }
+
+function convertTable2Array(hd) {
 	sel = window.getSelection();
 	if (sel.rangeCount && sel.getRangeAt) {
 		range = sel.getRangeAt(0);
 	}
-    var arrayCont = [];
-Array.from(range.commonAncestorContainer.children)
-	Array.from(range.commonAncestorContainer.children)
-	.forEach(itm=>{
-		arrayCont.push(JSON.parse('["'+itm.innerText.replace(/\t/g, '","')+'"]'))
-	})
-	return arrayCont;
-}
-convertTable2Array()
-
-
-//include header
-function convertTable2Array() {
-	sel = window.getSelection();
-	if (sel.rangeCount && sel.getRangeAt) {
-		range = sel.getRangeAt(0);
+	if(hd == 'hd'){ //include header
+		var arrayCont = Array.from(range.commonAncestorContainer.previousElementSibling.children).map(itm=>{return JSON.parse('["'+itm.innerText.replace(/\n/g, '').replace(/\t/g, '","')+'"]');});
+	}else{
+		var arrayCont = [];
 	}
-	var arrayCont = Array.from(range.commonAncestorContainer.previousElementSibling.children)
-.map(itm=>{
-		return JSON.parse('["'+itm.innerText.replace(/\n/g, '').replace(/\t/g, '","')+'"]')
-	});
-
-	Array.from(range.commonAncestorContainer.children)
-	.forEach(itm=>{
-		arrayCont.push(JSON.parse('["'+itm.innerText.replace(/\t/g, '","')+'"]'))
-	})
-return arrayCont;
-
+	Array.from(range.commonAncestorContainer.children).forEach(itm=>{arrayCont.push(JSON.parse('["'+itm.innerText.replace(/\t/g, '","')+'"]'))});
 	return arrayCont;
 }
 convertTable2Array()
