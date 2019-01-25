84832360fd781bf564a39d4230f3387d7e6eaccd
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-01-24 20:14:36 -0500

    Update downloadAsTextData.js

--- downloadAsTextData.js
+++ downloadAsTextData.js
@@ -1,20 +1,36 @@
 function downloadr(arr2D, filename) {
-    var data = arr2D.map(itm=>{	return itm.toString().replace(/$/, '\r'); }).toString().replace(/\r,/g, '\r');
-    var file = new Blob([data], {type: 'data:text/plain;charset=utf-8,'});
-    if (window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    } else {
-        var a = document.createElement('a'),
-		url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(() =>{
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);  
-        }, 10); 
-    }
+  if (/\.csv$/.test(filename) === true) {
+    var data = arr2D.map(itm => {
+      return itm.toString().replace(/$/, '\r');
+    }).toString().replace(/\r,/g, '\r');
+  }
+  if (/\.json$|.js$/.test(filename) === true) {
+    var data = arr2D;
+    var type = 'data:text/plain;charset=utf-8,';
+  } else {
+    var type = 'data:application/json;charset=utf-8,';
+  }
+  var data = arr2D.map(itm => {
+    return itm.toString().replace(/$/, '\r');
+  }).toString().replace(/\r,/g, '\r');
+  var file = new Blob([data], {
+    type: type
+  });
+  if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  } else {
+    var a = document.createElement('a'),
+      url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 10);
+  }
 }
 
-// downloadr(containArr, 'filename');
+// downloadr(containArr, 'filename.csv');
+// downloadr(containArr, 'filename.json');
