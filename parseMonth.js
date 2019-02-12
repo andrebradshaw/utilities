363b6876f21e11951b93a497a0b1872922fa47ae
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-02-12 12:34:05 -0500

    Create parseMonth.js

---
+++ parseMonth.js
@@ -0,0 +1,1 @@
+var parseMonth = (n) => n == 0 ? 'Jan'	: n == 1 ? 'Feb' 	: n == 2 ? 'Mar' 	: n == 3 ? 'Apr' 	: n == 4 ? 'May' 	: n == 5 ? 'Jun' 	: n == 6 ? 'Jul' 	: n == 7 ? 'Aug' 	: n == 8 ? 'Sep' 	: n == 9 ? 'Oct' 	: n == 10 ? 'Nov' 	: 'Dec'; 
