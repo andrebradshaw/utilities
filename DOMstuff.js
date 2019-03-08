67e6d89be6cc7050b2adb4f80c0523fa525ddb29
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-03-07 23:59:47 -0500

    Update DOMstuff.js

--- DOMstuff.js
+++ DOMstuff.js
@@ -3,3 +3,12 @@ var cn = (ob, nm) => ob.getElementsByClassName(nm);
 var tn = (ob, nm) => ob.getElementsByTagName(nm);
 var gi = (ob, nm) => ob.getElementById(nm);
 var delay = (ms) => new Promise(res => setTimeout(res, ms));
+var rando = (n) => Math.round(Math.random()+n);
+
+async function getDoc(s) {
+  var r = await fetch(s);
+  var t = await r.text();
+  var d = new DOMparser();
+  
+}  
+
