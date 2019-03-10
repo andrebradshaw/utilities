8a802b8c824c10edbc6666fc85cf5c4cb77d3867
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-03-09 23:05:52 -0500

    Update tsvTo2dArr.js

--- tsvTo2dArr.js
+++ tsvTo2dArr.js
@@ -1,1 +1,8 @@
-var tsvTo2dArr = (tsv) => csv.split(/\r\n|\n/).map(itm=> itm.match(/.+?(?=\t|$)/g).map(s=>s.replace(/\t/, '')));
+var tsvTo2dArr = (tsv) => tsv.split(/\r\n|\n/).map(itm=> itm.match(/.+?(?=\t|$)/g).map(s=>s.replace(/\t/, '')));
+// var arr2dToJSON = (arr) => 
+// var keysFromHead = (arr,temp) => 
+
+
+var csvArr = gi(document, "popup_textarea").value;
+var jdat = tsvTo2dArr(csvArr,[]);
+console.log(jdat);
