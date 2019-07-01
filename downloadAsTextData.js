9bb2ff4928e60d258fc049071207926ee8b4bba6
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-07-01 09:54:04 -0400

    Update downloadAsTextData.js

--- downloadAsTextData.js
+++ downloadAsTextData.js
@@ -1,29 +1,12 @@
 function downloadr(arr2D, filename) {
-  if (/\.csv$/.test(filename) === true) {
-	var data = '';
-	arr2D.forEach(row => {
-		var arrRow = '';
-		row.forEach(col => {
-			col ? arrRow = arrRow + col.toString().replace(/,/g, ' ') + ',' : arrRow = arrRow + ' ,';
-        });
-		data = data + arrRow + '\r'; 
-	});
-  }
-
-  if (/\.json$|.js$/.test(filename) === true) {
-    var data = JSON.stringify(arr2D);
-    var type = 'data:application/json;charset=utf-8,';
-  } else {
-	var type = 'data:text/plain;charset=utf-8,';
-  }
-  var file = new Blob([data], {
-    type: type
-  });
+  var data = /\.json$|.js$/.test(filename) ? JSON.stringify(arr2D) : arr2D.map(el=> el.reduce((a,b) => a+'\t'+b )).reduce((a,b) => a+'\r'+b);
+  var type = /\.json$|.js$/.test(filename) ? 'data:application/json;charset=utf-8,' : 'data:text/plain;charset=utf-8,';
+  var file = new Blob([data], {    type: type  });
   if (window.navigator.msSaveOrOpenBlob) {
     window.navigator.msSaveOrOpenBlob(file, filename);
   } else {
     var a = document.createElement('a'),
-      url = URL.createObjectURL(file);
+    url = URL.createObjectURL(file);
     a.href = url;
     a.download = filename;
     document.body.appendChild(a);
