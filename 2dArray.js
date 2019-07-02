d2e30df41a8630ac5effffd040d04dca7bf565bd
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-07-02 15:16:26 -0400

    Update 2dArray.js

--- 2dArray.js
+++ 2dArray.js
@@ -1,13 +1,11 @@
-function from1d2(arr, len){
-	var containArr = [];
-	for(i=0; i<arr.length; i=i+len){
-		var tArr = [];
-		for(d=i; d<(i+len); d++){
-			if(arr[d] !=undefined){
-				var d2 = tArr.push(arr[d]);
-        	}
-		}
-	containArr.push(tArr)
-}
-return containArr;
+function from1d2(arr, len) {
+  var containArr = [];
+  for (i = 0; i < arr.length; i = i + len) {
+    var tArr = [];
+    for (d = i; d < (i + len); d++) {
+      if (arr[d]) tArr.push(arr[d]);
+    }
+    containArr.push(tArr)
+  }
+  return containArr;
 }
