668b530b312b2a8db767deef24259f706ccb9067
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-08-13 15:52:16 -0400

    Update asciiConverter.js

--- asciiConverter.js
+++ asciiConverter.js
@@ -1,3 +1,3 @@
-var reChar = (s) => s.match(/&.+?;/g) ? s.match(/&.+?;/g).map(el=> [el,String.fromCharCode(/\d+/.exec(el)[0])]).map(m=> s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
+var reChar = (s) => s.match(/&#.+?;/g) && s.match(/&#.+?;/g).length > 0 ? s.match(/&#.+?;/g).map(el=> [el,String.fromCharCode(/\d+/.exec(el)[0])]).map(m=> s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
 reChar('T&#233;cnica en Administraci&#243;n y Finanzas')
 // output: "Técnica en Administración y Finanzas"
