602aa59b221d2085fc3f9a7f5285fa641df47652
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-08-23 10:17:58 -0400

    Update LinkedInNames.js

--- data cleansing/LinkedInNames.js
+++ data cleansing/LinkedInNames.js
@@ -1,3 +1,15 @@
 var firstNameCleanse = (str) => /(?<=\().+?(?=\))|(?<="|').+?(?="|')|(?<=[a-zA-Z]{1,3}\.\s)\S+/.exec(str) ? /(?<=\().+?(?=\))|(?<="|').+?(?="|')|(?<=[a-zA-Z]{1,3}\.\s)\S+/.exec(str)[0] : /\S+/.exec(str) ? /\S+/.exec(str)[0] : str;
 var lastNameCleanse = (str) => str[1] && /[a-z]/.test(str[1]) ? str.replace(/\s*[,-]\s*[A-Z\s-,]{3,}/,'').replace(/\s*,.+/, '') : str;
 var fixCase = (s)=> s.split(/\b-\b/).map(el=> el.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('-');
+
+
+/*
+ES5
+*/
+function fixCase(s){
+  return s.split(/\b-\b/).map(function(el){ 
+	return el.replace(/\w\S*/g, function(txt){ 
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    })
+  }).join('-');
+}
