6c4aaa59213880b6e3aa5d6c186c6c6c15eb7ca8
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-01 14:56:14 -0400

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -2,6 +2,7 @@ var rando = (n) => Math.round(Math.random() * n);
 var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
+var tn = (o, s) => o ? o.getElementsByTagName(s) : console.log(o);
 
 function genColor(n) {
   var s = '';
@@ -13,33 +14,49 @@ function genColor(n) {
 }
 
 function randomBalls() {
-  var circles = `<circle cx="70" cy="70" r="1" fill="#000"><animate attributeName="r" values="1; 120; 1;" begin="0s" dur="1.8s" repeatCount="0" /></circle>`;
-  for (var i = 0; i < 30; i++) {
-    var startPos = 70;
+  var circles = ``;
+  for (var i = 0; i < 550; i++) {
+    var startPos = 100;
     var startRad = rando(10) / 10;
     var endRad = rando(25) / 10;
-    var x = rando(120);
-    var z = rando(120);
+    var x = rando(180);
+    var y = rando(160);
     var color = `#${genColor(5)}`;
     var time = (rando(20) / 10);
-    circles += `<circle cx="${startPos}" cy="${startPos}" r="${startRad}" fill="${color}">
-    <animate attributeName="cy" values="${startPos}; ${z}; ${startPos};" begin="0s" dur="${time}s" repeatCount="0" />
-    <animate attributeName="r" values="${startRad}; ${endRad}; ${startRad};" begin="0s" dur="${time}s" repeatCount="0" />
-    <animate attributeName="cx" values="${startPos}; ${x}; ${startPos};" begin="0s" dur="${time}s" repeatCount="0" />
+    circles += `<circle cx="${x}" cy="${y}" r="${endRad}" fill="${color}">
+    <animate attributeName="cy" values="${startPos}; ${y-(y/10)}; ${y}; ${y};" begin="0s" dur="${time}s" repeatCount="0" />
+    <animate attributeName="r" values="${startRad}; ${endRad}; ${endRad-(endRad/10)}; ${startRad};" begin="0s" dur="${time}s" repeatCount="indefinite" />
+    <animate attributeName="cx" values="${startPos}; ${x-(x/10)}; ${x}; ${x};" begin="0s" dur="${time}s" repeatCount="0" />
   </circle>`;
   }
   return circles;
 }
 
+function killthis(){
+  var rad = parseFloat(this.getAttribute('r'));
+  parseFloat(this.getAttribute('r')) < 5 ? this.setAttribute('r', rad*3) : this.setAttribute('r', '1');
+//   this.outerHTML = '';
+}
+
+function resetPos(elm){
+  var circles = Array.from(tn(elm,'cirlce'));
+  for(var c=0; c<circles.length; c++){
+    var y = circles[c].getAttribute('cy');
+    var x = circles[c].getAttribute('cx');
+    circles[c].setAttribute('cy','100');
+    circles[c].setAttribute('cx','100');
+    circles[c].innerHTML = `<animate attributeName="cy" values="${y}; 100" begin="0s" dur="0.5s" repeatCount="0" />
+    <animate attributeName="cx" values="${x}; 100;" begin="0s" dur="0.5s" repeatCount="0" />`
+  }
+}
 async function celebrate() {
-  for (var i = 0; i < 5; i++) {
     var balls = randomBalls();
     var sv = ele('div');
-    attr(sv,'style','width: 300px; height: 300px;');
+    attr(sv,'style','position: fixed; z-index: 11111; top: 20%; left: 20%; width: 50%; height: 50%; clip-path: circle(50%);');
     document.body.appendChild(sv);
-    sv.innerHTML = `<svg>${balls}</svg>`;
-    await delay(1900);
-    sv.outerHTML = '';
-  }
+    sv.innerHTML = `<svg viewBox="0 0 200 200">${balls}</svg>`;
+    await delay(5900);
+resetPos(sv);
+//     sv.outerHTML = ''
 }
 celebrate()
