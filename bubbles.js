45ecad621472c303978a7ad5d1f584d3bb5dae4f
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-01 13:26:24 -0400

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -1,6 +1,7 @@
 var rando = (n) => Math.round(Math.random() * n);
 var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
+var attr = (o, k, v) => o.setAttribute(k, v);
 
 function genColor(n) {
   var s = '';
@@ -12,13 +13,13 @@ function genColor(n) {
 }
 
 function randomBalls() {
-  var circles = '';
+  var circles = `<circle cx="70" cy="70" r="1" fill="#000"><animate attributeName="r" values="1; 120; 1;" begin="0s" dur="1.8s" repeatCount="0" /></circle>`;
   for (var i = 0; i < 30; i++) {
-    var startPos = 30;
+    var startPos = 70;
     var startRad = rando(10) / 10;
     var endRad = rando(25) / 10;
-    var x = rando(60);
-    var z = rando(100);
+    var x = rando(120);
+    var z = rando(120);
     var color = `#${genColor(5)}`;
     var time = (rando(20) / 10);
     circles += `<circle cx="${startPos}" cy="${startPos}" r="${startRad}" fill="${color}">
@@ -31,11 +32,14 @@ function randomBalls() {
 }
 
 async function celebrate() {
-  var balls = randomBalls();
-  var sv = ele('div');
-  document.body.appendChild(sv);
-  sv.innerHTML = `<svg>${balls}</svg>`;
-  await delay(2000);
-  sv.outerHTML = '';
+  for (var i = 0; i < 5; i++) {
+    var balls = randomBalls();
+    var sv = ele('div');
+    attr(sv,'style','width: 300px; height: 300px;');
+    document.body.appendChild(sv);
+    sv.innerHTML = `<svg>${balls}</svg>`;
+    await delay(1900);
+    sv.outerHTML = '';
+  }
 }
 celebrate()
