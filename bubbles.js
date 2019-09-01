b7bad14314601d2b833b62acd366893ba1f231bb
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-09-01 17:04:35 -0400

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -3,6 +3,8 @@ var delay = (ms) => new Promise(res => setTimeout(res, ms));
 var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
 var tn = (o, s) => o ? o.getElementsByTagName(s) : console.log(o);
+var cn = (o, s) => o ? o.getElementsByClassName(s) : console.log(o);
+var gi = (o, s) => o ? o.getElementById(s) : console.log(o);
 
 function genColor(n) {
   var s = '';
@@ -14,16 +16,19 @@ function genColor(n) {
 }
 
 function randomBalls() {
+var colors = ['#ff1a0a','#ff430a','#ff6a00','#ffa463','#ffceab','#ffe0ab','#ffc157','#ffa50a','#ffc60a','#ffd64f','#ffe17d','#fff1c4','#fff1c4','#ffd036','#ffc300','#ffc300','#fff694','#ffee36','#fff836','#fffdd1','#d1ffff','#42ffff','#42ffff','#e0ffff','#e0f9ff','#12d1ff','#12d1ff','#d4f6ff','#d4ecff','#57b3ff','#0f93ff','#0f93ff','#0f67ff','#f2ebff','#733dd9','#5800ff','#5800ff','#b187ff','#c978ff','#c978ff','#fdedff','#ffedff','#ff00ff','#ff00c8','#ffe6fa','#ff009d','#ff0044','#ffc7d6','#ff002f','#ffdbe2','#ff2638'];
   var circles = ``;
-  for (var i = 0; i < 550; i++) {
+  for (var i = 0; i < 350; i++) {
     var startPos = 100;
-    var startRad = rando(10) / 10;
-    var endRad = rando(25) / 10;
-    var x = rando(180);
-    var y = rando(160);
+    var startRad = rando(6) / 10;
+    var endRad = rando(10) / 10;
+    var x = rando(160);
+    var y = rando(120);
+    var altColor = colors[rando((colors.length-1))];
     var color = `#${genColor(5)}`;
     var time = (rando(20) / 10);
-    circles += `<circle cx="${x}" cy="${y}" r="${endRad}" fill="${color}">
+    circles += `<circle class="bubbles" cx="${x}" cy="${y}" r="${startRad}" fill="${altColor}">
+    <animate attributeName="fill" values="${color}; ${altColor}; ${altColor};" begin="0s" dur="${time}s" repeatCount="0" />
     <animate attributeName="cy" values="${startPos}; ${y-(y/10)}; ${y}; ${y};" begin="0s" dur="${time}s" repeatCount="0" />
     <animate attributeName="r" values="${startRad}; ${endRad}; ${endRad-(endRad/10)}; ${startRad};" begin="0s" dur="${time}s" repeatCount="indefinite" />
     <animate attributeName="cx" values="${startPos}; ${x-(x/10)}; ${x}; ${x};" begin="0s" dur="${time}s" repeatCount="0" />
@@ -33,30 +38,32 @@ function randomBalls() {
 }
 
 function killthis(){
-  var rad = parseFloat(this.getAttribute('r'));
-  parseFloat(this.getAttribute('r')) < 5 ? this.setAttribute('r', rad*3) : this.setAttribute('r', '1');
-//   this.outerHTML = '';
+  this.outerHTML = '';
 }
 
-function resetPos(elm){
-  var circles = Array.from(tn(elm,'cirlce'));
+function resetPos(){
+  var circles = Array.from(cn(document,'bubbles'));
   for(var c=0; c<circles.length; c++){
-    var y = circles[c].getAttribute('cy');
-    var x = circles[c].getAttribute('cx');
-    circles[c].setAttribute('cy','100');
-    circles[c].setAttribute('cx','100');
-    circles[c].innerHTML = `<animate attributeName="cy" values="${y}; 100" begin="0s" dur="0.5s" repeatCount="0" />
-    <animate attributeName="cx" values="${x}; 100;" begin="0s" dur="0.5s" repeatCount="0" />`
+    var elm = circles[c];
+    var p = ['','-','','-','','-'];
+    var x = p[rando(6)];
+    var y = p[rando(6)];
+    elm.style.transform = `translate(${x+rando(60)}px, ${y+rando(60)}px)`;
+    elm.style.transition = `all ${rando(600)}ms ease-in-out`;
   }
 }
 async function celebrate() {
+  if(gi(document,'celebrationParent')) gi(document,'celebrationParent').outerHTML = '';
     var balls = randomBalls();
     var sv = ele('div');
-    attr(sv,'style','position: fixed; z-index: 11111; top: 20%; left: 20%; width: 50%; height: 50%; clip-path: circle(50%);');
+    attr(sv,'id','celebrationParent');
+    attr(sv,'style','position: fixed; z-index: 11111; top: 20%; left: 20%; width: 50%; height: 50%; background: #000; clip-path: circle(30%);');
     document.body.appendChild(sv);
-    sv.innerHTML = `<svg viewBox="0 0 200 200">${balls}</svg>`;
-    await delay(5900);
-resetPos(sv);
+    sv.innerHTML = `<svg viewBox="0 0 220 220">${balls}</svg>`;
+    sv.onclick = resetPos;
+    
 //     sv.outerHTML = ''
 }
+
+
 celebrate()
