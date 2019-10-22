fc4673fc6b12a4b5b77f2dc54cb5ba90e3ea3b28
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-10-22 13:40:07 -0400

    Update LinkedInNames.js

--- data cleansing/LinkedInNames.js
+++ data cleansing/LinkedInNames.js
@@ -1,7 +1,6 @@
 var firstNameCleanse = (str) => /(?<=\().+?(?=\))|(?<="|').+?(?="|')|(?<=[a-zA-Z]{1,3}\.\s)\S+/.exec(str) ? /(?<=\().+?(?=\))|(?<="|').+?(?="|')|(?<=[a-zA-Z]{1,3}\.\s)\S+/.exec(str)[0] : /\S+/.exec(str) ? /\S+/.exec(str)[0] : str;
 var lastNameCleanse = (str) => str[1] && /[a-z]/.test(str[1]) ? str.replace(/\s*[,-]\s*[A-Z\s-,]{3,}/,'').replace(/\s*,.+/, '') : str;
-var fixCase = (s)=> s.split(/\b-\b/).map(el=> el.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('-');
-
+var fixCase = (s)=> typeof s == 'string' && /^[A-Z]+$/.test(s.replace(/\W+/g, '').trim()) ? s.split(/\b-\b/).map(el=> el.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())).join('-') : s;
 
 /*
 ES5
