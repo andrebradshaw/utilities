255e2365c43217c2ec31c640a7db0310d2d1228a
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-12-18 12:40:04 -0500

    Create slav_latin.js

---
+++ slav_latin.js
@@ -0,0 +1,296 @@
+var slav_latin = [{
+  "slav": ["Иван", "Іван", "Іван", "Ян"],
+  "latin": ["Ivan", "Ivan", "Jan", "John"],
+  "origin": "Hebrew",
+  "comment": "equivalent to John"
+}, {
+  "slav": ["Илья", "Ілля", "Ілля"],
+  "latin": ["Ilya", "Illia", "Illia", "Elijah"],
+  "origin": "Hebrew",
+  "comment": "equivalent to Elijah"
+}, {
+  "slav": ["Николай", "Микола", "Мікалай", "Мікола"],
+  "latin": ["Nikolay", "Mykola", "Mikalaj", "Mikola", "Nicholas"],
+  "origin": "Greek",
+  "comment": "equivalent to Nicholas,meaning 'Victory (of the) People'"
+}, {
+  "slav": ["Боря", "Борис", "Борис", "Барыс"],
+  "latin": ["Borya", "Boris", "Borys", "Barys"],
+  "origin": "Bulgar",
+  "comment": "'fighter'"
+}, {
+  "slav": ["Вова", "Володя", "Владимир", "Володимир", "Уладзімір"],
+  "latin": ["Vova", "Volodya", "Vladimir", "Volodymyr", "Uladzimir"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Lord of the World','Lord of Peace'"
+}, {
+  "slav": ["Пётр", "Петро", "Пётр", "Пятро", "Пятрусь"],
+  "latin": ["Peter", "Pyotr", "Petro", "Piotr", "Piatro", "Piatruś"],
+  "origin": "Greek",
+  "comment": "equivalent to Peter"
+}, {
+  "slav": ["Андрей", "Андрій", "Андрэй"],
+  "latin": ["Andrey", "Andrii", "Andrej", "Andrew", "Andre", "Andrei"],
+  "origin": "Greek",
+  "comment": "equivalent to Andrew"
+}, {
+  "slav": ["Саша", "Саня", "Шура", "Сашко", "Лесь", "Александр", "Олександр", "Олекса", "Аляксандр"],
+  "latin": ["Sasha", "Sanya", "Shura", "Sashko", "Les", "Aleksandr", "Oleksandr", "Oleksa", "Aliaksandr", "Alex", "Alexander"],
+  "origin": "Greek",
+  "comment": "equivalent to Alexander"
+}, {
+  "slav": ["Филипп", "Пилип", "Піліп"],
+  "latin": ["Philip", "Pylyp", "Pilip"],
+  "origin": "Greek",
+  "comment": "from Greek Φίλιππος (Philippos),meaning 'fond of horses'."
+}, {
+  "slav": ["Димон", "Димка", "Дима", "Димочка", "Дмитрий", "Дмитро", "Дзмітры", "Зміцер"],
+  "latin": ["Dimon", "Dimka", "Dima", "Dimochka", "Demetrios", "Dmitry", "Dmytro", "Dzmitry", "Zmicier"],
+  "origin": "Greek",
+  "comment": "from Greek Δημήτριος (Demetrios),meaning 'of Demeter'"
+}, {
+  "slav": ["Серёжа", "Сергей", "Сергій", "Сяргей"],
+  "latin": ["Seryozha", "Sergey", "Serhii", "Siarhej", "Sergius"],
+  "origin": "Latin",
+  "comment": "from the Roman nomen (patrician family name) Sergius -> itself from a more ancient Etruscan name"
+}, {
+  "slav": ["Леонид", "Леонід", "Леанід", "Лявон"],
+  "latin": ["Leonidas", "Leonid", "Leonid", "Lieanid", "Liavon"],
+  "origin": "Greek",
+  "comment": "from Greek Leonidas,meaning 'Son of the Lion'"
+}, {
+  "slav": ["Витя", "Виктор", "Віктор", "Віктар"],
+  "latin": ["Vitya", "Viktor", "Viktor", "Viktar"],
+  "origin": "Latin",
+  "comment": "meaning 'Conqueror'"
+}, {
+  "slav": ["Юра", "Юрий", "Гоша", "Жора", "Георгий", "Георгiй", "Георгій"],
+  "latin": ["Yury", "Yura", "George", "Georgy", "Heorgii", "Hieorhij"],
+  "origin": "Greek",
+  "comment": "the analogues are Егор (Yegor),Юрий (Yury),equivalent to George"
+}, {
+  "slav": ["Павел", "Павло", "Павел", "Павал", "Паўло"],
+  "latin": ["Paul", "Pavel", "Pavlo", "Paval", "Paŭlo"],
+  "origin": "Latin",
+  "comment": "equivalent to Paul"
+}, {
+  "slav": ["Константин", "Костянтин", "Канстанцін", "Кастусь"],
+  "latin": ["Constantine", "Konstantin", "Kostiantyn", "Kanstancin", "Kastuś"],
+  "origin": "Latin",
+  "comment": "equivalent to Constantine"
+}, {
+  "slav": ["Кирилл", "Кирило", "Кірыл", "Кірыла"],
+  "latin": ["Cyril", "Kirill", "Kyrylo", "Kiryl", "Kiryla"],
+  "origin": "Greek",
+  "comment": "equivalent to Cyril"
+}, {
+  "slav": ["Вася", "Василий", "Василь", "Васіль", "Базыль"],
+  "latin": ["Ваsіl", "Vasya", "Vasily", "Vassili", "Vasyl", "Vasiĺ", "Bazyĺ"],
+  "origin": "Greek",
+  "comment": "equivalent to Ваsіl"
+}, {
+  "slav": ["Роман", "Роман", "Раман"],
+  "latin": ["Roman", "Roman", "Raman"],
+  "origin": "Latin",
+  "comment": "-"
+}, {
+  "slav": ["Владислава", "Влад", "Влада", "Владислав", "Владислав", "Уладзіслаў"],
+  "latin": ["Vlad", "Vlada", "Vladislav", "Vladyslav", "Uladzislaŭ"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Lord of Fame'"
+}, {
+  "slav": ["Слава", "Вячеслав", "В'ячеслав", "Вячаслаў"],
+  "latin": ["Slava", "Vyacheslav", "Viacheslav", "Viačaslaŭ"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Growing Fame'"
+}, {
+  "slav": ["Михаил", "Михайло", "Міхал", "Міхась"],
+  "latin": ["Michael", "Mikhail", "Mykhailo", "Michal", "Michaś"],
+  "origin": "Hebrew",
+  "comment": "equivalent to Michael"
+}, {
+  "slav": ["Олег", "Олег", "Алег"],
+  "latin": ["Helgi", "Oleg", "Oleh", "Alieh"],
+  "origin": "Old Norse",
+  "comment": "derivative from Scandinavian 'Helgi'"
+}, {
+  "slav": ["Игорь", "Ігор", "Ігар"],
+  "latin": ["Ingvar", "Igor", "Ihor", "Ihar"],
+  "origin": "Old Norse",
+  "comment": "derivative from Scandinavian 'Ingvar'"
+}, {
+  "slav": ["Максим", "Максим", "Максім"],
+  "latin": ["Maxim", "Maksym", "Maksim"],
+  "origin": "Latin",
+  "comment": "meaning 'Greatest'"
+}, {
+  "slav": ["Настя", "Стася", "Анастасия", "Анастасія", "Настасся", "Наста"],
+  "latin": ["Nastya", "Stasya", "Anastasia", "Anastasiia", "Nastassia", "Nasta"],
+  "origin": "Greek",
+  "comment": "from Greek Ἀναστασία (Anastasia) meaning 'she of the Resurrection'"
+}, {
+  "slav": ["Ангелина", "Ангеліна", "Ангеліна"],
+  "latin": ["Angelina", "Anhelina", "Angjelina"],
+  "origin": "Greek",
+  "comment": "From Greek Ἀγγελίνα (Angelina) meaning 'messenger'"
+}, {
+  "slav": ["Аня", "Анюта", "Нюта", "Нюша", "Áнна", "Анна", "Ганна", "Ганна"],
+  "latin": ["Hannah", "Anya", "Anyuta", "Nyuta", "Nyusha", "Anne", "Anna", "Anna", "Hanna", "Hanna"],
+  "origin": "Hebrew",
+  "comment": "equivalent to Anne or Hannah"
+}, {
+  "slav": ["Лена", "Алёна", "Елена", "Алёна", "Oленa", "Алена"],
+  "latin": ["Lena", "Alyona", "Helen", "Yelena", "Alyona", "Olena", "Aliena"],
+  "origin": "Greek",
+  "comment": "equivalent to Helen; in Russian Alyona can be both a pet version of Yelena and a name in its own right"
+}, {
+  "slav": ["Наталья", "Наталия", "Наталя", "Наталія", "Наталля"],
+  "latin": ["Natal'ya", "Natalie", "Natalia", "Nataliia", "Natallia"],
+  "origin": "Latin",
+  "comment": "equivalent to Natalie"
+}, {
+  "slav": ["Ольга", "Ольга", "Вольга"],
+  "latin": ["Helga", "Olga", "Olha", "Voĺha"],
+  "origin": "Old Norse",
+  "comment": "derivative from Scandinavian Helga"
+}, {
+  "slav": ["Саша", "Шура", "Леся", "Александра", "Олександра", "Alexandra", "Аляксандра"],
+  "latin": ["Sasha", "Shura", "Lesia", "Aleksandra", "Oleksandra", "Aliaksandra"],
+  "origin": "Greek",
+  "comment": "equivalent to Alexandra"
+}, {
+  "slav": ["Ксения", "Оксана", "Оксана", "Ксенія", "Аксана"],
+  "latin": ["Kseniya", "Oksana", "Oksana", "Ksienija", "Aksana"],
+  "origin": "Greek",
+  "comment": "in Russian Oksana is a separate name of the same origin"
+}, {
+  "slav": ["Катя", "Екатерина", "Катерина", "Кацярына"],
+  "latin": ["Katya", "Catherine", "Yekaterina", "Kateryna", "Kaciaryna"],
+  "origin": "Greek",
+  "comment": "equivalent to Catherine"
+}, {
+  "slav": ["Таня", "Татьяна", "Тетяна", "Тацяна", "Таццяна"],
+  "latin": ["Tatius", "Tatyana", "Tetiana", "Taciana", "Tacciana", "Tanya"],
+  "origin": "Latin",
+  "comment": "derivative from the Latinized name of the Sabine king Titus Tatius"
+}, {
+  "slav": ["Людмила", "Людмила", "Людміла"],
+  "latin": ["Lyudmila", "Liudmyla", "Ludmila"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Dear to the People'"
+}, {
+  "slav": ["Света", "Лана", "Светлана", "Світлана", "Святлана"],
+  "latin": ["Sveta", "Lana", "Svetlana", "Svitlana", "Sviatlana"],
+  "origin": "Slavonic",
+  "comment": "meaning 'The Shining One'"
+}, {
+  "slav": ["Юля", "Юлия", "Юлія", "Юлія"],
+  "latin": ["Yulya", "Julia", "Julie", "Yuliya", "Yuliia", "Julija"],
+  "origin": "Latin",
+  "comment": "equivalent to Julia or Julie"
+}, {
+  "slav": ["Вера", "Віра", "Вера"],
+  "latin": ["Faith", "Vera", "Vira", "Viera"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Faith'; a calque of the Greek Πίστη"
+}, {
+  "slav": ["Надежда", "Надія", "Надзея"],
+  "latin": ["Hope", "Nadezhda", "Nadiia", "Nadzieja"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Hope'; a calque of the Greek Ἐλπίς"
+}, {
+  "slav": ["Любовь", "Любов", "Любоў"],
+  "latin": ["Lyubov'", "Liubоv", "Liuboŭ"],
+  "origin": "Slavonic",
+  "comment": "meaning 'Love'; a calque of the Greek Ἀγάπη"
+}, {
+  "slav": ["Соня", "Софа", "София", "Софья", "Софія", "Соф'я"],
+  "latin": ["Sonya", "Sofa", "Sophia", "Sofiya", "Sofya", "Sofiia", "Sofja"],
+  "origin": "Greek",
+  "comment": "equivalent to Sophia,meaning 'Wisdom'."
+},{
+  "slav": ["Валерия", "Лера", "Лера"],
+  "latin": ["Valeriya", "Lera"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Валерий", "Валера"],
+  "latin": ["Valery", "Valera"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Валентин", "Валентина", "Валя"],
+  "latin": ["Valentin", "Valentina", "Valya"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Вадим", "Вадик", "Дима"],
+  "latin": ["Vadim", "Vadik", "Dima"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Станислав", "Стас"],
+  "latin": ["Stanislav", "Stas"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Дарья", "Даша"],
+  "latin": ["Darya", "Dasha"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Тамара", "Тома"],
+  "latin": ["Tamara", "Toma"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Ярослав", "Ярик"],
+  "latin": ["Yarik", "Yaroslav"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Елизавета", "Лиза"],
+  "latin": ["Yelizaveta", "Liza"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Анатолий", "Толя"],
+  "latin": ["Tolya", "Anatoly"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Алексей", "Алёша", "Лёша"],
+  "latin": ["Aleksey", "Alyosha", "Lyosha"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Григорий", "Гриша", "Гриць"],
+  "latin": ["Grisha", "Gritz", "Grigory"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Геннадий", "Гена"],
+  "latin": ["Gena", "Gennady"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Галина", "Галина", "Галя"],
+  "latin": ["Galya", "Galina"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Виктория", "Вика"],
+  "latin": ["Vika", "Viktoriya"],
+  "origin": "NA",
+  "comment": "short form"
+}, {
+  "slav": ["Евгений", "Женя", "Евгения"],
+  "latin": ["Yevgeniy", "Yevgeniya", "Zhenya"],
+  "origin": "NA",
+  "comment": "short form"
+}];
+
+function getLatinFromSlav(arr,slav){
+  return arr.filter(el=> el.slav.some(i=> i == slav)).map(el=> el.latin).flat();
+}
+getLatinFromSlav(slav_latin,'Евгений')
