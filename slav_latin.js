0f10dd226bb7779fd6a3089776616e20418c8eec
Andre Bradshaw <bradshaw.andre@gmail.com>
2019-12-20 09:23:10 -0500

    Update slav_latin.js

--- slav_latin.js
+++ slav_latin.js
@@ -290,15 +290,141 @@ var slav_latin = [{
   "comment": "short form"
 }];
 
-function getLatinFromSlav(arr,slav){
-  return arr.filter(el=> el.slav.some(i=> i == slav)).map(el=> el.latin).flat();
-}
-getLatinFromSlav(slav_latin,'Евгений')
+var reg = (o, n) => o ? o[n] : '';
+var cn = (o, s) => o ? o.getElementsByClassName(s) : console.log(o);
+var tn = (o, s) => o ? o.getElementsByTagName(s) : console.log(o);
+var gi = (o, s) => o ? o.getElementById(s) : console.log(o);
+var rando = (n) => Math.round(Math.random() * n);
+var unq = (arr) => arr.filter((e, p, a) => a.indexOf(e) == p);
+var delay = (ms) => new Promise(res => setTimeout(res, ms));
+var ele = (t) => document.createElement(t);
+var attr = (o, k, v) => o.setAttribute(k, v);
 
-function lookup(table, name){
-	var slav = table.filter(el=> el.slav.some(i=> i==name)) 
-	var latin = table.filter(el=> el.latin.some(i=> i==name)) 
-	if(slav.length) return slav.flat()
-	if(latin.length) return latin.flat()
+var a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
+
+var unqHsh = (a, o) => a.filter(i => o.hasOwnProperty(i) ? false : (o[i] = true));
+
+
+var cleanObject = (ob) =>
+  Object.entries(ob).reduce((r, [k, v]) => {
+    if (v) {
+      r[k] = v;
+      return r;
+    } else {
+      return r;
+    }
+  }, {});
+
+var svgs = {
+  close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
 }
 
+
+function aninCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
+  l1.style.transform = "translate(49px, 50px) rotate(45deg) translate(-49px, -50px)";
+  l1.style.transition = "all 233ms";
+  l2.style.transform = "translate(49px, 50px) rotate(135deg) translate(-49px, -50px)";
+  l2.style.transition = "all 233ms";
+}
+
+function anoutCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
+  l1.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+  l1.style.transition = "all 233ms";
+  l2.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+  l2.style.transition = "all 233ms";
+}
+
+function dragElement() {
+  var el = this.parentElement;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById(this.id)) document.getElementById(this.id).onmousedown = dragMouseDown;
+  else this.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    el.style.top = (el.offsetTop - pos2) + "px";
+    el.style.left = (el.offsetLeft - pos1) + "px";
+    el.style.opacity = "0.85";
+    el.style.transition = "opacity 700ms";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+    el.style.opacity = "1";
+  }
+}
+
+function slavLatinLookup(table, name){
+  var slav = table.filter(el=> el.slav.some(i=> i==name)) 
+  var latin = table.filter(el=> el.latin.some(i=> i==name)) 
+  if(slav.length) return slav.map(el=> [el.slav,el.latin]).flat(Infinity)
+  if(latin.length) return latin.map(el=> [el.slav,el.latin]).flat(Infinity)
+  else return null;
+}
+
+function createOptionHTML() {
+  if (gi(document, 'note_option_cont')) gi(document, 'note_option_cont').outerHTML = '';
+
+  var rect = this.getBoundingClientRect();
+
+  var cont = ele('div');
+  a(cont, [    ['id', 'note_option_cont'],    ['style', `position: fixed; top: ${rect.top}px; left: ${(rect.left-600)}px; z-index: ${new Date().getTime()}; width: 100px; border: 1px solid ${jdat.rgb}; border-radius: 0.4em; background: transparent; opacity: 0;`]  ]);
+  document.body.appendChild(cont);
+
+  var head = ele('div');
+  a(head, [    ['style', `display: grid; grid-template-columns: ${(cont.getBoundingClientRect().width - 30)}px 27px; width: 100%; background: #0a1114; border: 1.6px solid #0a1114; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move; padding: 4px;`]  ]);
+  cont.appendChild(head);
+  head.onmouseover = dragElement;
+
+  var txt = ele('span');
+  a(txt, [['style', `grid-column: 1/2; color: #fff; font-size: 1.1em; cursor: pointer; border-radius: 0.5em; padding: 4px; `]  ]);
+  head.appendChild(txt);
+  txt.innerText = jdat.username;
+//   txt.onclick = createChatLogsHTML;
+
+  var cls = ele('div');
+  a(cls, [    ['style', `grid-column: 2/2; width: 27px; height: 27px; cursor: pointer;`]  ]);
+  head.appendChild(cls);
+  cls.innerHTML = svgs.close;
+  cls.onmouseenter = aninCloseBtn;
+  cls.onmouseleave = anoutCloseBtn;
+  cls.onclick = () => cont.outerHTML = '';
+
+  var cbod = ele('div');
+  a(cbod, [    ['style', `width: 100%; border-bottom-right-radius: 0.4em; border-bottom-left-radius: 0.4em; background: #122026; padding 4px;`]  ]);
+  cont.appendChild(cbod);
+
+  var noteCont = ele('div');
+  a(noteCont, [    ['style', `padding: 4px;`]  ]);
+  cbod.appendChild(noteCont);
+
+  cont.style.width = '600px';
+//   cont.style.background = `${jdat.rgb}`;
+  cont.style.opacity = '1';
+  cont.style.transition = 'all 0.5s';
+
+}
+
+function createAltLangBtn(){
+  
+}
+
+slavLatinLookup(slav_latin,'Yevgeniy')
