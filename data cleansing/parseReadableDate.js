76dd4401b0a9b73465a73f5cd532ebaed9af8b61
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-02-27 15:31:29 -0500

    Update parseReadableDate.js

--- data cleansing/parseReadableDate.js
+++ data cleansing/parseReadableDate.js
@@ -1,4 +1,4 @@
 function parseReadableDate(t){
   var d = new Date(t);
-  return `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+  return `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${(d.getMinutes() > 9 ? d.getMinutes() : '0'+d.getMinutes())}`;
 }
