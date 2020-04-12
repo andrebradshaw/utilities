dd58992dbb325dff26243d682245ce7a13a2abfa
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-04-12 11:19:39 -0400

    Update multiJSON.js

--- data cleansing/multiJSON.js
+++ data cleansing/multiJSON.js
@@ -1,6 +1,6 @@
-function multiJSON(jdat){
+function multiJSON(jdat,filename){
   var is_array = typeof jdat == 'object' && Array.isArray(jdat) ? true : false;
-  
+  var sheet_name = filename.toLowerCase().replace(/^\W*|\W*$/g,'').replace(/\W+/g,'_');
   function processArrays(arr){
     var obj = {};
     arr.forEach(r=> {
@@ -29,15 +29,24 @@ function multiJSON(jdat){
     var array_items_are_all_objects = jdat.every(r=> Object.entries(r).every(kv=> typeof kv[1] == 'object') );
     if(array_items_are_all_objects){
       return processArrays(jdat);
+    }else{
+      var single_sheet = {};
+      single_sheet[sheet_name] = jdat;
+      return single_sheet; //this will return the original input. Assumed a flat array of items to convert to a Sheet
     }
   }else{
     var filtered = Object.entries(jdat).filter(kv=> typeof kv[1] == 'object' && Array.isArray(kv[1]));
     if(filtered.length){
-      return processArrays(jdat);
+      var flattened = filtered.map(kv=> kv[1]).flat()
+      return processArrays(flattened);
     }else{
       var dat_obs = Object.entries(jdat).filter(kv=> typeof kv[1] == 'object');
-//       var twice_filtered = dat_obs.map(d=> Object.entries(d[1]).filter(kv=> ))
+      if(dat_obs.length){
+//         var twice_filtered = dat_obs.map(d=> Object.entries(d[1]).filter(kv=> kv[]))
+      }else{
+        return false;
+      }
     }
   }
 }
-flattenJSON(fileArray)
+//multiJSON(fileArray,'test_sheet')
