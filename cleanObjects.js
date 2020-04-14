475bac90f038a1e026c3bbb2c2cb6981c5c53bf4
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-04-14 10:39:39 -0400

    Update cleanObjects.js

--- cleanObjects.js
+++ cleanObjects.js
@@ -1,10 +1,9 @@
 var cleanObject = (ob) => 
   Object.entries(ob).reduce((r, [k, v]) => {
-    if(v != null && v != undefined && ( typeof v == 'boolean' || typeof v == 'string' || typeof v == 'symbol' || typeof v == 'number' || typeof v == 'function' || (typeof v == 'object'  && ((Array.isArray(v) && v.length) || (Array.isArray(v) != true)) ) ) ) { 
+    if(v != null && v != undefined && v != "" && ( typeof v == 'boolean' || typeof v == 'string' || typeof v == 'symbol' || typeof v == 'number' || typeof v == 'function' || (typeof v == 'object'  && ((Array.isArray(v) && v.length) || (Array.isArray(v) != true)) ) ) ) { 
       r[k] = v; 
       return r;
     } else { 
      return r; 
     }
   }, {});
-//if array is empty, delete
