2b7be2eb892a974006dd1d0b86ec8bfa40d6e0ef
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-04-18 20:55:08 -0400

    Update everyNth.js

--- everyNth.js
+++ everyNth.js
@@ -2,11 +2,10 @@
 This function gives the user an easy way to specify an interation value to return. 
 this will give us a true response every nth cycle
 
-for(var i=0; 1<500; i++){
-  if( everyNth(1,108) ) console.log(i)
+for(var i=0; i<500; i++){
+  if( everyNth(i,108) ) console.log(i)
 }
 would log: 0, 108, 216, 324, 432
 */
 
-var everyNth = (i,n) => ( ( i / Math.floor(n/2) ) % 2) == 0;
-
+var everyNth = (i,n) => /\./.test((i/n).toString()) === false;
