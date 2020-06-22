146a6dd63122a3e8359e2df1270f74d449671811
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-06-22 10:28:51 -0400

    Update nohtmlEntities.js

--- data cleansing/nohtmlEntities.js
+++ data cleansing/nohtmlEntities.js
@@ -2,5 +2,6 @@
 function reg(o,n){return o ? o[n] : ''}
 function reChar(s) {	return typeof s == 'string' && s.match(/&#\d+;/g) && s.match(/&#\d+;/g).length > 0 ? s.match(/&#\d+;/g).map(function(el){return [el, String.fromCharCode(reg(/&#(\d+);/.exec(el),1))]}).map(function(m) {return s = s.replace(new RegExp(m[0], 'i'), m[1])}).pop() : s;}
 //ES6
+var reg = (o, n) => o ? o[n] : '';
 var reChar = (s) => typeof s == 'string' && s.match(/&#\d+;/g) && s.match(/&#\d+;/g).length > 0 ? s.match(/&#\d+;/g).map(el => [el, String.fromCharCode(reg(/(?<=&#).+?(?=;)/.exec(el),0))]).map(m => s = s.replace(new RegExp(m[0], 'i'), m[1])).pop() : s;
 var noHtmlEntities = (s) => typeof s == 'string' ? s.replace(/&amp;/g, '&').replace(/&quot;/g, '"').replace(/&apos;/g, "'").replace(/&nbsp;/g, ' ') : s;
