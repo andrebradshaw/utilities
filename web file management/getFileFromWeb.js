08eafe533e11f6b71530c583a63e55419e141a92
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-07-13 22:20:46 -0400

    Create getFileFromWeb.js

---
+++ web file management/getFileFromWeb.js
@@ -0,0 +1,18 @@
+async function parseURI(d){
+  var reader = new FileReader();    /* https://developer.mozilla.org/en-US/docs/Web/API/FileReader */
+  reader.readAsDataURL(d);          /* https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL */
+  return new Promise((res,rej)=> {  /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise */
+    reader.onload = (e) => {        /* https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onload */
+      res(e.target.result)
+    }
+  })
+} 
+
+async function getDataBlob(url){
+  var res = await fetch(url);
+  var blob = await res.blob();
+  var uri = await parseURI(blob);
+  console.log(/.{0,100}/.exec(uri));
+}
+
+getDataBlob(your_url);
