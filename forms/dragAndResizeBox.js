f7cea80aff9737fa65853223f78b271a9e58609f
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-07-31 00:10:33 -0400

    Update dragAndResizeBox.js

--- forms/dragAndResizeBox.js
+++ forms/dragAndResizeBox.js
@@ -6,21 +6,35 @@ var ele = (t) => document.createElement(t);
 var attr = (o, k, v) => o.setAttribute(k, v);
 var a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
 
+function aninCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
+  l1.style.transform = "translate(49px, 50px) rotate(45deg) translate(-49px, -50px)";
+  l1.style.transition = "all 1033ms";
+  l2.style.transform = "translate(49px, 50px) rotate(135deg) translate(-49px, -50px)";
+  l2.style.transition = "all 433ms";
+}
+
+function anoutCloseBtn() {
+  var l1 = tn(this, 'path')[0];
+  var l2 = tn(this, 'path')[1];
+  l1.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+  l1.style.transition = "all 633ms";
+  l2.style.transform = "translate(49px, 50px) rotate(225deg) translate(-49px, -50px)";
+  l2.style.transition = "all 333ms";
+}
+
 function dragElement() {
   var el = this.parentElement;
-
   var pos1 = 0,    pos2 = 0,    pos3 = 0,    pos4 = 0;
-
   if (document.getElementById(this.id)) document.getElementById(this.id).onmousedown = dragMouseDown;
   else this.onmousedown = dragMouseDown;
-
   function dragMouseDown(e) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
-
   function elementDrag(e) {
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
@@ -31,7 +45,6 @@ function dragElement() {
     el.style.opacity = "0.85";
     el.style.transition = "opacity 700ms";
   }
-
   function closeDragElement() {
     document.onmouseup = null;
     document.onmousemove = null;
@@ -46,44 +59,36 @@ function adjustElementSize(){
   var foot = this.parentElement;
   var head_height = cont.firstChild.getBoundingClientRect().height;
   var foot_height = foot.getBoundingClientRect().height;
-
   var pos1 = 0,    pos2 = 0,    pos3 = 0,    pos4 = 0;
   var width = parseFloat(cont.style.width.replace(/px/,''));
   var height = parseFloat(cont.getBoundingClientRect().height);
-  
   if (document.getElementById(this.id)) document.getElementById(this.id).onmousedown = dragMouseDown;
   else this.onmousedown = dragMouseDown;
-
-
   function dragMouseDown(e) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     document.onmousemove = elementDrag;
   }
-
   function elementDrag(e) {
     cont.style.width = width - (pos3 - e.clientX) + 'px';
     main.style.width = width - (pos3 - e.clientX) + 'px';
-
+    cbod.style.height = width - (pos3 - e.clientX) + 'px';
     cont.style.height = height - (pos4 - e.clientY) + 'px';
     main.style.height = height - (pos4 - e.clientY) + 'px';
-    cbod.style.height = (height - (pos4 - e.clientY))-(head_height+foot_height) + 'px';
-
+    cbod.style.height = (height - (pos4 - e.clientY)) - (head_height+foot_height) + 'px';
     var rect = main.getBoundingClientRect();
     var edge = 15;
     cbod.style.height = `${(height - (pos4 - e.clientY))-(head_height+foot_height)}px`;
     cbod.style.width = `${(width - (pos3 - e.clientX))-(head_height+foot_height)}px;`;
-
-    a(foot, [['style', `display: grid; grid-template-columns: ${(rect.width - (edge+3))}px ${edge}px; background: #0a1114; border: 1.6px solid #0a1114; border-bottom-left-radius: 0.4em; border-bottom-right-radius: 0.4em; height: ${edge+4}px;`]]);
-    cont.style.opacity = "0.85";
-    cont.style.transition = "opacity 700ms";
+    a(foot, [['style', `display: grid; grid-template-columns: ${(rect.width - (edge+4))}px ${edge}px; background: #0a1114; border: 1.6px solid #0a1114; border-bottom-left-radius: 0.4em; border-bottom-right-radius: 0.4em; height: ${edge+4}px;`]]);
+    cont.style.opacity = '0.75';
+    cont.style.transition = 'opacity 200ms';
   }
-
   function closeDragElement() {
     document.onmouseup = null;
     document.onmousemove = null;
-    cont.style.opacity = "1";
+    cont.style.opacity = '1';
   }
 
 }
@@ -91,7 +96,7 @@ function adjustElementSize(){
 
 
 var svgs = {
-    close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(0.85, 0.85)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
+    close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(1, 1)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#e21212" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
     resize: `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1000.000000 1000.000000" version="1.0">
 <g stroke="none" fill="#43de6d" transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)">
 <path d="M9235 9969 c-31 -17 -9164 -9151 -9181 -9181 -8 -15 -14 -49 -14 -76 0 -38 6 -57 29 -88 34 -46 535 -544 571 -568 28 -18 110 -22 143 -5 31 16 9165 9148 9183 9181 8 15 14 49 14 76 0 38 -6 57 -29 88 -34 46 -535 544 -571 568 -28 18 -114 21 -145 5z"/>
@@ -103,10 +108,11 @@ var svgs = {
 }; 
 
 function testHTML(){
-  if(gi(document,'test_container')) gi(document,'test_container').outerHTML = '';
+  var main_cont_id = 'test_container', cbod_bg_color = '#c1c1d1', head_text = 'Test Header';
+  if(gi(document,main_cont_id)) gi(document,main_cont_id).outerHTML = '';
 
   var cont = ele('div');
-  a(cont,[['id','test_container'],['style', `position: fixed; top: 100px; left: 100px; z-index: ${new Date().getTime()}; width: 500px; border: 1px solid #0a1114; border-radius: 0.45em; background: #FFF;`]]);
+  a(cont,[['id',main_cont_id],['style', `position: fixed; top: 100px; left: 100px; z-index: ${new Date().getTime()}; width: 500px; border: 1px solid #0a1114; border-radius: 0.45em; background: #FFF;`]]);
   document.body.appendChild(cont);
 
   var head = ele('div');
@@ -117,13 +123,15 @@ function testHTML(){
   var txt = ele('div');
   a(txt, [['style', `color: #fff; font-size: 1.3em; border-radius: 0.5em; color: #fff; text-align: center;`]]);
   head.appendChild(txt);
-  txt.innerText = 'Test Header';
+  txt.innerText = head_text;
 
   var cls = ele('div');
-  a(cls, [['style', `width: 29px; height: 29px; cursor: pointer;`]]);
+  a(cls, [['style', `cursor: pointer;`]]);
   head.appendChild(cls);
   cls.innerHTML = svgs.close;
   cls.onclick = () => cont.outerHTML = '';
+  cls.onmouseenter = aninCloseBtn;
+  cls.onmouseleave = anoutCloseBtn;
 
   var cont_rect = cont.getBoundingClientRect();
   var edge = 15;
@@ -133,12 +141,12 @@ function testHTML(){
   cont.appendChild(mainbod);
 
   var cbod = ele('div');
-  a(cbod,[['id','content_body_'],['style',`background: #c1c1d1; padding: 8px;`]]);
+  a(cbod,[['id','content_body_'],['style',`background: ${cbod_bg_color}; padding: 8px;`]]);
   mainbod.appendChild(cbod);
   cbod.innerHTML = `This element would be where you would house the entirety of your content. <br>Changing any other elements would cause the resizing to break.`;
   
   var footer = ele('div');
-  a(footer, [['style', `display: grid; grid-template-columns: ${(cont_rect.width - (edge+3))}px ${edge}px; background: #0a1114; border: 1.6px solid #0a1114; border-bottom-left-radius: 0.4em; border-bottom-right-radius: 0.4em; height: ${edge+4}px;`]]);
+  a(footer, [['style', `display: grid; grid-template-columns: ${(cont_rect.width - (edge+4))}px ${edge}px; background: #0a1114; border: 1.6px solid #0a1114; border-bottom-left-radius: 0.4em; border-bottom-right-radius: 0.4em; height: ${edge+4}px;`]]);
   mainbod.appendChild(footer);
   
   var footertext = ele('div');
