15baa8ba444a8e05df619569ead6f3aedc5dc41f
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-08-27 16:17:47 -0400

    Update unq.js

--- data cleansing/unq.js
+++ data cleansing/unq.js
@@ -1,1 +1,14 @@
 var unqHsh = (a,o) => a.filter(i=> o.hasOwnProperty(i) ? false : (o[i] = true));
+//
+
+function unqKey(array,key)
+  var q = [];
+  var map = new Map();
+  for (const item of array) {
+    if(!map.has(item[key])){
+        map.set(item[key], true);
+        q.push(item);
+    }
+  }
+  return q;
+}
