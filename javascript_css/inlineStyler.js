89524b9f5642232e71c94c816d20a13784aa1920
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-09-25 11:50:39 -0400

    Create inlineStyler.js

---
+++ javascript_css/inlineStyler.js
@@ -0,0 +1,11 @@
+function inlineStyler(css,elm){
+  Object.entries(JSON.parse(
+  css.replace(/(?<=:)\s*(\b|\B)(?=.+?;)/g,'"')
+    .replace(/(?<=:\s*.+?);/g,'",')
+    .replace(/[a-zA-Z-]+(?=:)/g, k=> k.replace(/^\b/,'"').replace(/\b$/,'"'))
+    .replace(/\s*,\s*\}/g,'}')
+  )).forEach(kv=> { elm.style[kv[0]] = kv[1]});
+}
+var style = `{text-align: center; font-size: 1.3em; padding: 6px; border: 1px solid #transparent; border-radius: 0.4em; background: #26bd7e; color: #fff; width: 100%; cursor: pointer;}`;
+
+inlineStyler(style,document.getElementById('ID_HERE_head'))
