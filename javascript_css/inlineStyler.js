810422ff641707a4dbc5615acecc755ee904667a
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-09-25 11:53:03 -0400

    Update inlineStyler.js

--- javascript_css/inlineStyler.js
+++ javascript_css/inlineStyler.js
@@ -1,4 +1,4 @@
-function inlineStyler(css,elm){
+function inlineStyler(elm,css){
   Object.entries(JSON.parse(
   css.replace(/(?<=:)\s*(\b|\B)(?=.+?;)/g,'"')
     .replace(/(?<=:\s*.+?);/g,'",')
@@ -8,4 +8,4 @@ function inlineStyler(css,elm){
 }
 var style = `{text-align: center; font-size: 1.3em; padding: 6px; border: 1px solid #transparent; border-radius: 0.4em; background: #26bd7e; color: #fff; width: 100%; cursor: pointer;}`;
 
-inlineStyler(style,document.getElementById('ID_HERE_head'))
+inlineStyler(document.getElementById('ID_HERE_head'),style)
