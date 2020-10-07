5d920eaff9c761ccb439cfd78019301a2faf4b82
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-10-07 14:22:09 -0400

    Update unqKey.js

--- data cleansing/unqKey.js
+++ data cleansing/unqKey.js
@@ -9,3 +9,25 @@ function unqKey(array,key){
   }
   return q;
 }
+
+/*
+   NESTED VERSION => takes up to 4 levels of object dot notation. no arrays.
+*/
+function unqKey(array,key){
+  var q = [];
+  var map = new Map();
+  for (const item of array) {
+    if(!map.has(keymapper(item,key))){
+        map.set(keymapper(item,key), true);
+        q.push(item);
+    }
+  }
+  return q;
+}
+
+function keymapper(obj,key){
+  var k = key.split(/\./);
+  return k.length == 4 ? obj[k[0]][k[1]][k[2]][k[3]] : k.length == 3 ? obj[k[0]][k[1]][k[2]] : k.length == 2 ? obj[k[0]][k[1]] : k.length == 1 ? obj[k[0]] : false;
+}
+
+var test_merge = unqKey(fileArray,'memberProfileResolutionResult.entityUrn')
