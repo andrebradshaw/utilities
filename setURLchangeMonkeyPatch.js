f7fe1689ea638bdd9dcedf7971479a7ba05bd372
Andre Bradshaw <bradshaw.andre@gmail.com>
2020-12-29 20:07:35 -0500

    Update setURLchangeMonkeyPatch.js

--- setURLchangeMonkeyPatch.js
+++ setURLchangeMonkeyPatch.js
@@ -1,4 +1,4 @@
-const quickli_page_change_monitor = 'quickli_page_change_monitor';
+var quickli_page_change_monitor = 'quickli_page_change_monitor';
 function setURLchangeMonkeyPatch(id){
     const reg = (o, n) => o ? o[n] : '';
     const cn = (o, s) => o ? o.getElementsByClassName(s) : null;
@@ -11,24 +11,24 @@ function setURLchangeMonkeyPatch(id){
     a(monitor,[['id',id],['current_url_displayed',window.location.href],['style','display: fixed;']]);
     document.body.appendChild(monitor);
 }
-function hasPageUrlChanged(id){
+function hasPageUrlChanged(id,url){
     const gi = (o, s) => o ? o.getElementById(s) : null;
-    const url = window.location.href;
     const monitor_elm = gi(document,id);
     const url_onlast_state = monitor_elm?.getAttribute('current_url_displayed');
+console.log([url_onlast_state,url]);
     if(url_onlast_state != url) {
         monitor_elm.setAttribute('current_url_displayed',url);
         return true;
     }else return false;
 }
 function monitorURLChanges(){
-    let has_changed = hasPageUrlChanged(quickli_page_change_monitor);
-    if(has_changed) // Do some stuff
-    else console.log('no change');
+    let has_changed = hasPageUrlChanged(quickli_page_change_monitor,window.location.href);
+    if(has_changed === true) console.log('has changed',window.location.href);
+    else console.log('no change',window.location.href);
 }
 async function initClientOptionsMenu(){
     const bg_res = await requestFromBackground();
     if(bg_res.run_these) bg_res.run_these.forEach(i=> Object.values(i).forEach(v=> eval(v)));
 }
 setURLchangeMonkeyPatch(quickli_page_change_monitor);
-window.onmousemove = monitorURLChanges;
+document.body.onmousemove = monitorURLChanges;
