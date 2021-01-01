f3105d92e7893a9faba392096bd5a848c72e9de4
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-01-01 13:59:20 -0500

    Update setURLchangeMonkeyPatch.js

--- setURLchangeMonkeyPatch.js
+++ setURLchangeMonkeyPatch.js
@@ -1,4 +1,4 @@
-var quickli_page_change_monitor = 'quickli_page_change_monitor';
+const page_change_monitor = 'page_change_monitor';
 function setURLchangeMonkeyPatch(id){
     const reg = (o, n) => o ? o[n] : '';
     const cn = (o, s) => o ? o.getElementsByClassName(s) : null;
@@ -15,20 +15,21 @@ function hasPageUrlChanged(id,url){
     const gi = (o, s) => o ? o.getElementById(s) : null;
     const monitor_elm = gi(document,id);
     const url_onlast_state = monitor_elm?.getAttribute('current_url_displayed');
-console.log([url_onlast_state,url]);
+// console.log([url_onlast_state,url]);
     if(url_onlast_state != url) {
         monitor_elm.setAttribute('current_url_displayed',url);
         return true;
     }else return false;
 }
 function monitorURLChanges(){
-    let has_changed = hasPageUrlChanged(quickli_page_change_monitor,window.location.href);
-    if(has_changed === true) console.log('has changed',window.location.href);
-    else console.log('no change',window.location.href);
+    let has_changed = hasPageUrlChanged(page_change_monitor,window.location.href);
+    if(has_changed === true) {
+//do some stuff
+      console.log('has changed',window.location.href);
+    }
+//     else console.log('no change',window.location.href);
 }
-async function initClientOptionsMenu(){
-    const bg_res = await requestFromBackground();
-    if(bg_res.run_these) bg_res.run_these.forEach(i=> Object.values(i).forEach(v=> eval(v)));
-}
-setURLchangeMonkeyPatch(quickli_page_change_monitor);
+
+setURLchangeMonkeyPatch(page_change_monitor);
 document.body.onmousemove = monitorURLChanges;
+
