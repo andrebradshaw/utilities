9d4f90c08f6ee552a92e5d665ef7b77c53087e45
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-01-07 14:55:11 -0500

    Update searchWithNearPermutations.js

--- search tools/searchWithNearPermutations.js
+++ search tools/searchWithNearPermutations.js
@@ -1,50 +1,44 @@
-var parseStringAsXset = (s) => s
-.split(/\s+\band\b\s+|(?<!\s+and\b)\s+\(|\)\s+(?!\band\b)/i)
-    .map(el=> 
-        el.split(/\s+\bor\b\s+/i).map(ii=> 
-            ii.replace(/\s*\)\s*/g,'')
-            .replace(/\s*\(\s*/g,'')
-            .replace(/\s+/g,'.{0,3}')
-            .replace(/"/g,'\\b')
-            .replace(/\*/g,'\\w*')
-            .replace(/\*\*\*/g,'.{0,60}'))
-                .reduce((a,b)=> a+'|'+b)).filter(el=> el);
-
-function permutateNear(input,joiner){
-  var nearx = /(?<=\|)\S+?(?=\||$)/g;
-  var base = input.replace(nearx, '').replace(/[\|]+/g, '|');
-  var near_or = input.match(nearx) ? input.match(nearx).map(str=> {
-    var arr = str.split(/~/);
-    if(arr.length > 5){
-      return str.replace(/[~]+/,'.');
-    }else{
-      var cont = [];
-      var containArr = [];
-      function comboLoop(arr, cont){
-        if (arr.length == 0) {
-          var row = cont.join(joiner);
-          containArr.push(row)
-        }
-        for (var i = 0; i < arr.length; i++) {
-          var x = arr.splice(i, 1);
-          cont.push(x);
+function convertBoolSearch(search){
+    const parseStringAsXset = (s) => s.split(/\s+\band\b\s+|(?<!\s+and\b)\s+\(|\)\s+(?!\band\b)/i).map(el=>el.split(/\s+\bor\b\s+/i).map(ii=>ii.replace(/\s*\)\s*/g,'').replace(/\s*\(\s*/g,'').replace(/\s+/g,'.{0,3}').replace(/"/g,'\\b').replace(/\*/g,'\\w*').replace(/\*\*\*/g,'.{0,60}')).reduce((a,b)=> a+'|'+b)).filter(el=> el);
+    function permutateNear(input,joiner){
+      var nearx = /(?<=\||^)\S+?(?=\||$)/g;
+      var base = input.replace(nearx, '').replace(/[\|]+/g, '|');
+      var near_or = input.match(nearx) ? input.match(nearx).map(str=> {
+        var arr = str.split(/~/);
+        if(arr.length > 5){
+          return str.replace(/[~]+/,'.');
+        }else{
+          var cont = [];
+          var containArr = [];
+          function comboLoop(arr, cont){
+            if (arr.length == 0) {
+              var row = cont.join(joiner);
+              containArr.push(row)
+            }
+            for (var i = 0; i < arr.length; i++) {
+              var x = arr.splice(i, 1);
+              cont.push(x);
+              comboLoop(arr, cont);
+              cont.pop();
+              arr.splice(i, 0, x);
+            }
+          }
           comboLoop(arr, cont);
-          cont.pop();
-          arr.splice(i, 0, x);
+          return containArr.reduce((a,b)=> a+'|'+b);
         }
-      }
-      comboLoop(arr, cont);
-      return containArr.reduce((a,b)=> a+'|'+b);
+      }).flat().reduce((a,b)=> a+'|'+b) : '';
+      return base + near_or;
     }
-  }).flat().reduce((a,b)=> a+'|'+b) : '';
-  return base + near_or;
+    function buildSearchSet(str){
+      if(str){
+          var set = parseStringAsXset(str);
+          var xset = set.map(r=> permutateNear(r,'.{0,39}')).map(r=> tryRegExp(r.replace(/^\||\|$/g,''),'i'));
+          return xset;
+      }else{return null}
+    }
+    function tryRegExp(s,f){
+        try{return new RegExp(s,f)}
+        catch(err){return err}
+    }
+    return buildSearchSet(search);
 }
-
-function buildSearchSet(str){
-  var set = parseStringAsXset(str);
-  var xset = set.map(r=> permutateNear(r,'.{0,3}')).map(r=> new RegExp(r,'i'));
-  console.log(xset);
-  return xset;
-}
-
-buildSearchSet(`("test * this" or run that )  and run the jewels  (one OR "two person"~hat~frog four OR director~*~engineer)`)
