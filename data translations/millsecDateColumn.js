301bd0766c95b2785945b5d02acb18085095e25b
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-01-19 18:07:54 -0500

    Create millsecDateColumn.js

---
+++ data translations/millsecDateColumn.js
@@ -0,0 +1,6 @@
+function dateString(d){
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var date = new Date(d);
+  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+`millsec_column`.split(/\n/).map(d=> dateString(parseInt(d)))
