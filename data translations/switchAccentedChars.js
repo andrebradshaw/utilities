fa6953c3c3cb95ce648fb10f8d53707a863e3179
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-01-18 19:46:29 -0500

    Create switchAccentedChars.js

---
+++ data translations/switchAccentedChars.js
@@ -0,0 +1,12 @@
+var switchAccentedChars = (s) => s
+  .replace(/á|à|â|ä|ã|å/g, 'a')
+  .replace(/Á|À|Â|Ä|Ã/g,'A')
+  .replace(/æ|é|è|ê|ë/g,'e')
+  .replace(/É|È|Ê|Ë/g,'E')
+  .replace(/í|ì|î|ï/g,'i')
+  .replace(/ñ|Ñ/g,'n')
+  .replace(/ó|ò|ô|ö|õ|ø|œ/g,'o')
+  .replace(/Ó|Ò|Ô|Ö|Õ|Ø|Œ/g,'O')
+  .replace(/ß/g,'ss')
+  .replace(/Ú|Ù|Û|Ü/g,'U')
+  .replace(/ú|ù|û|ü/g,'u');
