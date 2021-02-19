37c6e9e892c9de010ba45421b6b5da26a5ab58c4
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-02-18 20:40:38 -0500

    Create unqCount.js

---
+++ data translations/unqCount.js
@@ -0,0 +1,20 @@
+function unqCount(records,key){
+    let keyvals = [];
+    Object.entries(records.reduce((obj, b) => {
+        obj[b] = ++obj[b] || 1;
+        return obj;
+      }, {})).forEach(kv=> {
+        let k = {};
+        k[key] = kv[0];
+        k['count'] = kv[1];
+        keyvals.push(k);
+      });
+      return keyvals;
+}
+
+unqCount(
+    [
+new Date().getTime(),new Date().getTime(),new Date().getTime()
+    ],
+    'timestamp'
+)
