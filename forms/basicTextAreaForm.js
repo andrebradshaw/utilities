ef8cc9f0efc27138402f368ea411c72e863b22ba
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-02-27 13:08:13 -0500

    Update basicTextAreaForm.js

--- forms/basicTextAreaForm.js
+++ forms/basicTextAreaForm.js
@@ -1,3 +1,4 @@
+
 function createUploadForm(){
     var reg = (o, n) => o ? o[n] : '';
     var cn = (o, s) => o ? o.getElementsByClassName(s) : null;
@@ -94,55 +95,29 @@ function createUploadForm(){
             }    
         }
 
-
-    //#fa3e3e //f5bb41
     var svgs = {
         close: `<svg x="0px" y="0px" viewBox="0 0 100 100"><g style="transform: scale(1, 1)" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"><g transform="translate(2, 2)" stroke="#fa3e3e" stroke-width="8"><path d="M47.806834,19.6743435 L47.806834,77.2743435" transform="translate(49, 50) rotate(225) translate(-49, -50) "/><path d="M76.6237986,48.48 L19.0237986,48.48" transform="translate(49, 50) rotate(225) translate(-49, -50) "/></g></g></svg>`,
-        resize: `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1000.000000 1000.000000" version="1.0">
-    <g stroke="none" fill="#43de6d" transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)">
-    <path d="M9235 9969 c-31 -17 -9164 -9151 -9181 -9181 -8 -15 -14 -49 -14 -76 0 -38 6 -57 29 -88 34 -46 535 -544 571 -568 28 -18 110 -22 143 -5 31 16 9165 9148 9183 9181 8 15 14 49 14 76 0 38 -6 57 -29 88 -34 46 -535 544 -571 568 -28 18 -114 21 -145 5z"/>
-    <path d="M5923 4093 c-1911 -1908 -3479 -3476 -3484 -3485 -5 -9 -9 -38 -9 -64 l0 -48 228 -228 228 -228 53 0 53 0 3478 3472 c1914 1909 3482 3478 3485 3485 3 8 5 35 5 61 l0 46 -228 228 -228 228 -53 0 -53 0 -3475 -3467z"/>
-    <path d="M7042 2957 l-2442 -2442 0 -45 0 -45 213 -213 212 -212 45 0 45 0 2443 2443 2442 2442 0 45 0 45 -213 213 -212 212 -45 0 -45 0 -2443 -2443z"/>
-    <path d="M8088 1922 l-1478 -1477 0 -45 c0 -44 1 -45 178 -222 177 -178 178 -178 222 -178 l45 0 1472 1473 1473 1472 0 55 0 56 -173 172 c-172 171 -174 172 -218 172 l-44 0 -1477 -1478z"/>
-    </g>
-    </svg>`,
-        resize_hover: `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 1000.000000 1000.000000" preserveAspectRatio="xMidYMid meet">
-    <g transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)" fill="#43de6d" stroke="none">
-    <path d="M5318 4622 l-3798 -3797 0 -59 0 -60 312 -314 c172 -172 325 -320 340 -328 15 -8 49 -14 75 -14 l48 0 3797 3798 3798 3797 0 59 0 60 -312 314 c-172 172 -325 320 -340 328 -15 8 -49 14 -75 14 l-48 0 -3797 -3798z"/>
-    <path d="M6763 3147 l-2483 -2482 0 -50 0 -49 268 -268 268 -268 49 0 50 0 2482 2483 2483 2482 0 50 0 49 -268 268 -268 268 -49 0 -50 0 -2482 -2483z"/>
-    <path d="M8058 1902 l-1268 -1267 0 -50 0 -50 248 -247 247 -248 50 0 50 0 1267 1268 1268 1267 0 50 0 50 -248 247 -247 248 -50 0 -50 0 -1267 -1268z"/>
-    </g>
-    </svg>`,
+        resize: `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1000.000000 1000.000000" version="1.0">    <g stroke="none" fill="#43de6d" transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)">    <path d="M9235 9969 c-31 -17 -9164 -9151 -9181 -9181 -8 -15 -14 -49 -14 -76 0 -38 6 -57 29 -88 34 -46 535 -544 571 -568 28 -18 110 -22 143 -5 31 16 9165 9148 9183 9181 8 15 14 49 14 76 0 38 -6 57 -29 88 -34 46 -535 544 -571 568 -28 18 -114 21 -145 5z"/>    <path d="M5923 4093 c-1911 -1908 -3479 -3476 -3484 -3485 -5 -9 -9 -38 -9 -64 l0 -48 228 -228 228 -228 53 0 53 0 3478 3472 c1914 1909 3482 3478 3485 3485 3 8 5 35 5 61 l0 46 -228 228 -228 228 -53 0 -53 0 -3475 -3467z"/>    <path d="M7042 2957 l-2442 -2442 0 -45 0 -45 213 -213 212 -212 45 0 45 0 2443 2443 2442 2442 0 45 0 45 -213 213 -212 212 -45 0 -45 0 -2443 -2443z"/>    <path d="M8088 1922 l-1478 -1477 0 -45 c0 -44 1 -45 178 -222 177 -178 178 -178 222 -178 l45 0 1472 1473 1473 1472 0 55 0 56 -173 172 c-172 171 -174 172 -218 172 l-44 0 -1477 -1478z"/>    </g>    </svg>`,
+        resize_hover: `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 1000.000000 1000.000000" preserveAspectRatio="xMidYMid meet">    <g transform="translate(0.000000,1000.000000) scale(0.100000,-0.100000)" fill="#43de6d" stroke="none">    <path d="M5318 4622 l-3798 -3797 0 -59 0 -60 312 -314 c172 -172 325 -320 340 -328 15 -8 49 -14 75 -14 l48 0 3797 3798 3798 3797 0 59 0 60 -312 314 c-172 172 -325 320 -340 328 -15 8 -49 14 -75 14 l-48 0 -3797 -3798z"/>    <path d="M6763 3147 l-2483 -2482 0 -50 0 -49 268 -268 268 -268 49 0 50 0 2482 2483 2483 2482 0 50 0 49 -268 268 -268 268 -49 0 -50 0 -2482 -2483z"/>    <path d="M8058 1902 l-1268 -1267 0 -50 0 -50 248 -247 247 -248 50 0 50 0 1267 1268 1268 1267 0 50 0 50 -248 247 -247 248 -50 0 -50 0 -1267 -1268z"/>    </g>    </svg>`,
     }; 
 
     function createDraggableResizableContainer(edit) {
-        const {
-          main_cont_id,
-          cbod_bg_color,
-          head_bg_color,
-          head_text_color,
-          head_text,
-          width,
-          left,
-          top,
-          height,
-        } = edit;
+        const {main_cont_id,cbod_bg_color,head_bg_color,head_text_color,head_text,width,left,top,height} = edit;
         if (gi(document, main_cont_id)) gi(document, main_cont_id).outerHTML = '';
 
         let cont = ele('div');
         a(cont, [['dragme', 'true'],['id', main_cont_id],]); 
         inlineStyler(cont,`{position: fixed; top: ${top}px; left: ${left}px; z-index: ${new Date().getTime()}; max-height: ${height}px; width: ${width}px; border: 0px solid #2b2b2b; border-radius: 0.45em; background: #2b2b2b; font-family: sans-serif;}`);
-    //"Lucida Console", Monaco, monospace
         document.body.appendChild(cont);
 
         let head = ele('div');
         a(head, [['id',main_cont_id+'_head']]);
-        inlineStyler(head,`{display: grid; grid-template-columns: 1fr 29px; background: ${head_bg_color}; border: 1.6px solid #0a1114; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move;}`)
+        inlineStyler(head,`{display: grid; grid-template-columns: 1fr 29px; background: ${head_bg_color}; border: 1.6px solid #0a1114; border-top-left-radius: 0.4em; border-top-right-radius: 0.4em; cursor: move;}`);
         cont.appendChild(head);
         head.onmouseover = dragElement;
 
         let txt = ele('div');
-        inlineStyler(txt, `{font-size: 1.3em; border-radius: 0.5em; color: ${head_text_color}; text-align: center;}`)
+        inlineStyler(txt, `{font-size: 1.3em; border-radius: 0.5em; color: ${head_text_color}; text-align: center;}`);
         head.appendChild(txt);
         txt.innerText = head_text;
 
@@ -204,9 +179,9 @@ function createUploadForm(){
 
     let cont_obj = {
         main_cont_id: 'ID_HERE',
-        head_bg_color: '#2b2b2b', //#e6d30b
+        head_bg_color: '#2b2b2b',
         cbod_bg_color: '#f5f5f2',
-        head_text_color: '#f2f2f2', //#2b2b2b
+        head_text_color: '#f2f2f2',
         head_text: 'HEADER TEXT HERE',
         width: window.innerWidth > 799 ? window.innerWidth * 0.6 : window.innerWidth * 0.8,
         left: (window.innerWidth - (window.innerWidth > 799 ? window.innerWidth * 0.6 : window.innerWidth * 0.8)) * 0.5,
