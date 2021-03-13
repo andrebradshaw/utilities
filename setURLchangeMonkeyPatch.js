6e0ccf1b6b656e57140bea1162c1fd80e02bf6c5
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-03-13 17:56:54 -0500

    Update setURLchangeMonkeyPatch.js

--- setURLchangeMonkeyPatch.js
+++ setURLchangeMonkeyPatch.js
@@ -1,35 +1,19 @@
-const page_change_monitor = 'page_change_monitor';
-function setURLchangeMonkeyPatch(id){
-    const reg = (o, n) => o ? o[n] : '';
-    const cn = (o, s) => o ? o.getElementsByClassName(s) : null;
-    const tn = (o, s) => o ? o.getElementsByTagName(s) : null;
-    const gi = (o, s) => o ? o.getElementById(s) : null;
-    const ele = (t) => document.createElement(t);
-    const attr = (o, k, v) => o.setAttribute(k, v);
-    const a = (l, r) => r.forEach(a => attr(l, a[0], a[1]));
-    const monitor = ele('div');
-    a(monitor,[['id',id],['current_url_displayed',window.location.href],['style','display: fixed;']]);
-    document.body.appendChild(monitor);
+function dosomething(){
+    console.log(this);
 }
-function hasPageUrlChanged(id,url){
-    const gi = (o, s) => o ? o.getElementById(s) : null;
-    const monitor_elm = gi(document,id);
-    const url_onlast_state = monitor_elm?.getAttribute('current_url_displayed');
-// console.log([url_onlast_state,url]);
-    if(url_onlast_state != url) {
-        monitor_elm.setAttribute('current_url_displayed',url);
-        return true;
-    }else return false;
-}
-function monitorURLChanges(){
-    let has_changed = hasPageUrlChanged(page_change_monitor,window.location.href);
-    if(has_changed === true) {
-//do some stuff
-      console.log('has changed',window.location.href);
+const page_change_monitor_object = {
+    id: 'page_change_monitor',
+    fn: dosomething,
+    page_context: {
+        href: window.location.href
     }
-//     else console.log('no change',window.location.href);
+};
+function monitorURLChanges(){
+    const url = window.location.href;
+    if(page_change_monitor_object?.page_context?.href != url) {
+        page_change_monitor_object.fn();
+        console.log('has changed',url);
+        page_change_monitor_object['page_context']['href'] = url;
+    }
 }
-
-setURLchangeMonkeyPatch(page_change_monitor);
 document.body.onmousemove = monitorURLChanges;
-
