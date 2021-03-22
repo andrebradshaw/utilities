e4b3655d3c12f6c09d89c957c9925cdf22984775
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-03-22 00:25:13 -0400

    Update commaNumber.js

--- data translations/commaNumber.js
+++ data translations/commaNumber.js
@@ -1,4 +1,4 @@
 function commaNumber(n){
     const everyNth = (i,n) => /\./.test((i/n).toString()) === false && i != 0;
-    return n.split('').reverse().map((t,i,r)=> everyNth(i,3) ? t+',' : t).reverse().reduce((a,b)=> a+b);
+    return n.toString().split('').reverse().map((t,i,r)=> everyNth(i,3) ? t+',' : t).reverse().reduce((a,b)=> a+b);
 }
