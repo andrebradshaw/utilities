d2751380bd7fc833fc2e258ceba1e76d76b73939
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-04-27 10:29:12 -0400

    Update resSetCaretPosToEnd.js

--- forms/resSetCaretPosToEnd.js
+++ forms/resSetCaretPosToEnd.js
@@ -1,11 +1,21 @@
-function resSetCaretPosToEnd(id) {
-    var el = document.getElementById(id);
-    var range = document.createRange();
-    var sel = window.getSelection();
-		var text_elms = Array.from(el.childNodes).filter(i=> i.textContent);
-    var last_elm = text_elms[(text_elms.length-1)]
-    range.setStart(last_elm, (last_elm.textContent.length-1))
-    range.collapse(true);
-    sel.removeAllRanges();
-    sel.addRange(range);
+function resSetCaretPosToEnd(id) { //editable div
+	var el = document.getElementById(id);
+	var range = document.createRange();
+	var sel = window.getSelection();
+	var text_elms = Array.from(el.childNodes).filter(i=> i.textContent);
+	var last_elm = text_elms[(text_elms.length-1)]
+	range.setStart(last_elm, (last_elm.textContent.length-1))
+	range.collapse(true);
+	sel.removeAllRanges();
+	sel.addRange(range);
+}
+
+
+function setCaretPositionInTextbox(id) { //input/textarea
+	const elm = document.getElementById(id);
+	var caret_pos = elm.value.length;
+	if(elm != null) {
+	    elm.focus();
+	    elm.setSelectionRange(caret_pos, caret_pos);
+	}
 }
