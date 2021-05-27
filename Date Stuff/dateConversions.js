02e5639a01c89bd747a8fd735e3491cb5edb6684
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-05-27 10:17:21 -0400

    Create dateConversions.js

---
+++ Date Stuff/dateConversions.js
@@ -0,0 +1,10 @@
+function yearMonthDate(d){
+  var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+  var date = new Date(d);
+  return `${date.getFullYear()}-${months[date.getMonth()]}-${date.getDate()}`;
+}
+function dateMonthYear(d){
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var date = new Date(d);
+  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
