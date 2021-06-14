069a6764430630795aa35c7bcf107c90e3822bad
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-06-14 15:19:25 -0400

    Update switchAccentedChars.js

--- data translations/switchAccentedChars.js
+++ data translations/switchAccentedChars.js
@@ -1,26 +1,14 @@
-var switchAccentedChars = (s) => s
-  .replace(/à|á|â|ä|æ|ã|å|ā/g, 'a')
-  .replace(/À|Á|Â|Ä|Æ|Ã|Å|Ā/g,'A')
-  .replace(/è|é|ê|ë|ē|ė|ę/g,'e')
-  .replace(/È|É|Ê|Ë|Ē|Ė|Ę/g,'E')
-  .replace(/Î|Ï|Í|Ī|Į|Ì/g,'I')
-  .replace(/î|ï|í|ī|į|ì/g,'i')
-  .replace(/ñ|Ñ|Ń/g,'N')
-  .replace(/ñ|ń/g,'n')
-  .replace(/ô|ö|ò|ó|œ|ø|ō|õ/g,'o')
-  .replace(/Ô|Ö|Ò|Ó|Œ|Ø|Ō|Õ/g,'O')
-  .replace(/ß|ś|š/g,'s')
-  .replace(/Ś|Š/g,'S')
-  .replace(/Û|Ü|Ù|Ú|Ū/g,'U')
-  .replace(/û|ü|ù|ú|ū/g,'u')
-  .replace(/ÿ/g,'y')
-  .replace(/Ÿ/g,'Y')
-  .replace(/ł/g,'l')
-  .replace(/Ł/g,'L')
-  .replace(/Ç|Ć|Č/g,'C')
-  .replace(/ç|ć|č/g,'c')
-  .replace(/Ž|Ź|Ż/g,'Z')
-  .replace(/ž|ź|ż/g,'z');
+var accents = [{        codes:[224, 225, 226, 228, 230, 227, 229, 257],        char:'a'    },{        codes:[192, 193, 194, 196, 198, 195, 197, 256],        char:'A'    },{        codes:[232, 233, 234, 235, 275, 279, 281],        char:'e'    },{        codes:[200, 201, 202, 203, 274, 278, 280],        char:'E'    },{        codes:[206, 207, 205, 298, 302, 204],        char:'I'    },{        codes:[238, 239, 237, 299, 303, 236],        char:'i'    },{        codes:[209, 323],        char:'N'    },{        codes:[241, 324],        char:'n'    },{        codes:[244, 246, 242, 243, 339, 248, 333, 245],        char:'o'    },{        codes:[212, 214, 210, 211, 338, 216, 332, 213],        char:'O'    },{        codes:[223, 347, 353],        char:'s'    },{        codes:[346, 352],        char:'S'    },{        codes:[219, 220, 217, 218, 362],        char:'U'    },{        codes:[251, 252, 249, 250, 363],        char:'u'    },{        codes:[255],        char:'y'    },{        codes:[376],        char:'Y'    },{        codes:[322],        char:'l'    },{        codes:[321],        char:'L'    },{        codes:[199, 262, 268],        char:'C'    },{        codes:[231, 263, 269],        char:'c'    },{        codes:[381, 377, 379],        char:'Z'    },{        codes:[382, 378, 380],        char:'z'    }].map(r=> {
+    return {
+        ...r,
+        ...{x:  new RegExp(r.codes.map(d=> String.fromCharCode(d)).reduce((a,b)=> a+'|'+b),'g')}
+    }
+});
+function switchAccentedChars(s){
+     accents.forEach(i=> {
+        s = s.replace(i.x,i.char)
+    })
+    return s;
+}
 
-switchAccentedChars('àŃ')
-// "aN"
+switchAccentedChars('AîôóúÇoĆopČ')
