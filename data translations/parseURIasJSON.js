6c4e1c369bad4b0b9cf0a9ba490f8900895a1b5b
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-06-26 14:29:12 -0400

    Update parseURIasJSON.js

--- data translations/parseURIasJSON.js
+++ data translations/parseURIasJSON.js
@@ -1,4 +1,8 @@
-    function parseURIasJSON(url,obj) {
-        if(url.match(/(?<=\?|\&)\S+?(?=\&|$)/g)) url.match(/(?<=\?|\&)\S+?(?=\&|$)/g).map(r=> r ? r.split(/\=/) : [[]]).forEach(r=> obj[r[0]] = r[1])
-        return obj;
+function parseURIasJSON(url,obj) {
+    function tryJSON(s){
+        try{ return JSON.parse(decodeURIComponent(s))}
+        catch(err){ console.log(err); return s};
     }
+    if(url.match(/(?<=\?|\&)\S+?(?=\&|$)/g)) url.match(/(?<=\?|\&)\S+?(?=\&|$)/g).map(r=> r ? r.split(/\=/) : [[]]).forEach(r=> obj[r[0]] = tryJSON(r[1]))
+    return obj;
+}
