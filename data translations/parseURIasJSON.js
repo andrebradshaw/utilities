42a767875dca141515565b7aa37e06d514466841
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-06-26 14:25:05 -0400

    Create parseURIasJSON.js

---
+++ data translations/parseURIasJSON.js
@@ -0,0 +1,4 @@
+    function parseURIasJSON(url,obj) {
+        if(url.match(/(?<=\?|\&)\S+?(?=\&|$)/g)) url.match(/(?<=\?|\&)\S+?(?=\&|$)/g).map(r=> r ? r.split(/\=/) : [[]]).forEach(r=> obj[r[0]] = r[1])
+        return obj;
+    }
