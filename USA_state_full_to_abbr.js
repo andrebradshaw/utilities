f604d851478aebd46fc7a0df540f8c556844d5c1
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-08-03 10:27:03 -0400

    Update USA_state_full_to_abbr.js

--- USA_state_full_to_abbr.js
+++ USA_state_full_to_abbr.js
@@ -1,8 +1,9 @@
 function usStateSwitch(str){
   var arr = [["Alabama","AL"],["Alaska","AK"],["Arizona","AZ"],["Arkansas","AR"],["California","CA"],["Colorado","CO"],["Connecticut","CT"],["Delaware","DE"],["District of Columbia","DC"],["Florida","FL"],["Georgia","GA"],["Hawaii","HI"],["Idaho","ID"],["Illinois","IL"],["Indiana","IN"],["Iowa","IA"],["Kansas","KS"],["Kentucky","KY"],["Louisiana","LA"],["Maine","ME"],["Maryland","MD"],["Massachusetts","MA"],["Michigan","MI"],["Minnesota","MN"],["Mississippi","MS"],["Missouri","MO"],["Montana","MT"],["Nebraska","NE"],["Nevada","NV"],["New Hampshire","NH"],["New Jersey","NJ"],["New Mexico","NM"],["New York","NY"],["North Carolina","NC"],["North Dakota","ND"],["Ohio","OH"],["Oklahoma","OK"],["Oregon","OR"],["Pennsylvania","PA"],["Rhode Island","RI"],["South Carolina","SC"],["South Dakota","SD"],["Tennessee","TN"],["Texas","TX"],["Utah","UT"],["Vermont","VT"],["Virginia","VA"],["Washington","WA"],["West Virginia","WV"],["Wisconsin","WI"],["Wyoming","WY"],["American Samoa","AS"],["Guam","GU"],["Northern Mariana Islands","MP"],["Puerto Rico","PR"],["U.S. Virgin Islands","VI"],["U.S. Minor Outlying Islands","UM"],["Micronesia","FM"],["Marshall Islands","MH"],["Palau","PW"]];
   for(var i=0; i<arr.length; i++){
-    if(str == arr[i][0] || str == arr[i][1]){
+    if(str?.toLowerCase() == arr[i][0]?.toLowerCase() || str?.toLowerCase() == arr[i][1]?.toLowerCase()){
       return str.length > 2 ? arr[i][1] : arr[i][0];
     }
   }
 }
+usStateSwitch('ga')
