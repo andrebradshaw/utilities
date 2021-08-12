b95a01e250da3672c06d1ea744f56aa685c4377b
Andre Bradshaw <bradshaw.andre@gmail.com>
2021-08-12 11:57:10 -0400

    Update normalizeJSON.js

--- data translations/normalizeJSON.js
+++ data translations/normalizeJSON.js
@@ -2,7 +2,7 @@ function normalizeJSON(arr){
     return arr.map(r=> {
         return Object.entries(r).map(kv=> {
             let obj = {};
-            let key = kv[0]?.replace(/%/g,'perc')?.replace(/-/g,'to')?.replace(/\W+/g,'_')?.replace(/^\b(?=\d)/,'_')?.replace(/_$/,'')?.toLowerCase();
+            let key = kv[0]?.replace(/#/g,'number')?.replace(/%/g,'percent')?.replace(/-/g,'to')?.replace(/\W+/g,'_')?.replace(/^\b(?=\d)/,'_')?.replace(/_$/,'')?.toLowerCase();
             let val = typeof kv[1] == 'string' ? (/^[\d\.,]+$|^-[\d\.,]+$/.test(kv[1]?.replace(/,/g,'')) ? parseFloat(kv[1]) : /^\$[\d,\.]+$/.test(kv[1]) ? parseFloat(kv[1]?.replace(/\$|,/g,'')) : /[\d\.,]+%$/.test(kv[1]) ? (parseFloat(kv[1]?.replace(/%|,/g,'')) / 100) : kv[1] ) : kv[1];
             obj[key] = val;
             return obj;
