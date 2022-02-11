ae4909974e3551b1074cab164559ec51639e99a8
Andre Bradshaw <bradshaw.andre@gmail.com>
2022-02-11 14:08:25 -0500

    Create country_code_map.js

---
+++ country_code_map.js
@@ -0,0 +1,322 @@
+var tsv = `Country	Alpha-2 code	Alpha-3 code	Numeric
+Afghanistan	AF	AFG	004
+Albania	AL	ALB	008
+Algeria	DZ	DZA	012
+American Samoa	AS	ASM	016
+Andorra	AD	AND	020
+Angola	AO	AGO	024
+Anguilla	AI	AIA	660
+Antarctica	AQ	ATA	010
+Antigua and Barbuda	AG	ATG	028
+Argentina	AR	ARG	032
+Armenia	AM	ARM	051
+Aruba	AW	ABW	533
+Australia	AU	AUS	036
+Austria	AT	AUT	040
+Azerbaijan	AZ	AZE	031
+Bahamas	BS	BHS	044
+Bahrain	BH	BHR	048
+Bangladesh	BD	BGD	050
+Barbados	BB	BRB	052
+Belarus	BY	BLR	112
+Belgium	BE	BEL	056
+Belize	BZ	BLZ	084
+Benin	BJ	BEN	204
+Bermuda	BM	BMU	060
+Bhutan	BT	BTN	064
+Bolivia	BO	BOL	068
+Bonaire, Sint Eustatius and Saba	BQ	BES	535
+Bosnia and Herzegovina	BA	BIH	070
+Botswana	BW	BWA	072
+Bouvet Island	BV	BVT	074
+Brazil	BR	BRA	076
+British Indian Ocean Territory	IO	IOT	086
+Brunei Darussalam	BN	BRN	096
+Bulgaria	BG	BGR	100
+Burkina Faso	BF	BFA	854
+Burundi	BI	BDI	108
+Cabo Verde	CV	CPV	132
+Cambodia	KH	KHM	116
+Cameroon	CM	CMR	120
+Canada	CA	CAN	124
+Cayman Islands	KY	CYM	136
+Central African Republic	CF	CAF	140
+Chad	TD	TCD	148
+Chile	CL	CHL	152
+China	CN	CHN	156
+Christmas Island	CX	CXR	162
+Cocos Islands	CC	CCK	166
+Keeling Islands	CC	CCK	166
+Colombia	CO	COL	170
+Comoros (the)	KM	COM	174
+Congo (the Democratic Republic of the)	CD	COD	180
+Congo	CG	COG	178
+Cook Islands	CK	COK	184
+Costa Rica	CR	CRI	188
+Croatia	HR	HRV	191
+Cuba	CU	CUB	192
+Curaçao	CW	CUW	531
+Cyprus	CY	CYP	196
+Czechia	CZ	CZE	203
+Côte d'Ivoire	CI	CIV	384
+Denmark	DK	DNK	208
+Djibouti	DJ	DJI	262
+Dominica	DM	DMA	212
+Dominican Republic	DO	DOM	214
+Ecuador	EC	ECU	218
+Egypt	EG	EGY	818
+El Salvador	SV	SLV	222
+Equatorial Guinea	GQ	GNQ	226
+Eritrea	ER	ERI	232
+Estonia	EE	EST	233
+Eswatini	SZ	SWZ	748
+Ethiopia	ET	ETH	231
+Falkland Islands	FK	FLK	238
+Faroe Islands	FO	FRO	234
+Fiji	FJ	FJI	242
+Finland	FI	FIN	246
+France	FR	FRA	250
+French Guiana	GF	GUF	254
+French Polynesia	PF	PYF	258
+French Southern Territories (the)	TF	ATF	260
+Gabon	GA	GAB	266
+Gambia	GM	GMB	270
+Georgia	GE	GEO	268
+Germany	DE	DEU	276
+Ghana	GH	GHA	288
+Gibraltar	GI	GIB	292
+Greece	GR	GRC	300
+Greenland	GL	GRL	304
+Grenada	GD	GRD	308
+Guadeloupe	GP	GLP	312
+Guam	GU	GUM	316
+Guatemala	GT	GTM	320
+Guernsey	GG	GGY	831
+Guinea	GN	GIN	324
+Guinea-Bissau	GW	GNB	624
+Guyana	GY	GUY	328
+Haiti	HT	HTI	332
+Heard Island and McDonald Islands	HM	HMD	334
+Holy See	VA	VAT	336
+Honduras	HN	HND	340
+Hong Kong	HK	HKG	344
+Hungary	HU	HUN	348
+Iceland	IS	ISL	352
+India	IN	IND	356
+Indonesia	ID	IDN	360
+Iran	IR	IRN	364
+Iraq	IQ	IRQ	368
+Ireland	IE	IRL	372
+Isle of Man	IM	IMN	833
+Israel	IL	ISR	376
+Italy	IT	ITA	380
+Jamaica	JM	JAM	388
+Japan	JP	JPN	392
+Jersey	JE	JEY	832
+Jordan	JO	JOR	400
+Kazakhstan	KZ	KAZ	398
+Kenya	KE	KEN	404
+Kiribati	KI	KIR	296
+Korea	KR	KOR	410
+Kuwait	KW	KWT	414
+Kyrgyzstan	KG	KGZ	417
+Lao People's Democratic Republic	LA	LAO	418
+Latvia	LV	LVA	428
+Lebanon	LB	LBN	422
+Lesotho	LS	LSO	426
+Liberia	LR	LBR	430
+Libya	LY	LBY	434
+Liechtenstein	LI	LIE	438
+Lithuania	LT	LTU	440
+Luxembourg	LU	LUX	442
+Macao	MO	MAC	446
+Madagascar	MG	MDG	450
+Malawi	MW	MWI	454
+Malaysia	MY	MYS	458
+Maldives	MV	MDV	462
+Mali	ML	MLI	466
+Malta	MT	MLT	470
+Marshall Islands	MH	MHL	584
+Martinique	MQ	MTQ	474
+Mauritania	MR	MRT	478
+Mauritius	MU	MUS	480
+Mayotte	YT	MYT	175
+Mexico	MX	MEX	484
+Micronesia	FM	FSM	583
+Moldova	MD	MDA	498
+Monaco	MC	MCO	492
+Mongolia	MN	MNG	496
+Montenegro	ME	MNE	499
+Montserrat	MS	MSR	500
+Morocco	MA	MAR	504
+Mozambique	MZ	MOZ	508
+Myanmar	MM	MMR	104
+Namibia	NA	NAM	516
+Nauru	NR	NRU	520
+Nepal	NP	NPL	524
+Netherlands	NL	NLD	528
+New Caledonia	NC	NCL	540
+New Zealand	NZ	NZL	554
+Nicaragua	NI	NIC	558
+Niger	NE	NER	562
+Nigeria	NG	NGA	566
+Niue	NU	NIU	570
+Norfolk Island	NF	NFK	574
+Northern Mariana Islands	MP	MNP	580
+Norway	NO	NOR	578
+Oman	OM	OMN	512
+Pakistan	PK	PAK	586
+Palau	PW	PLW	585
+Palestine, State of	PS	PSE	275
+Panama	PA	PAN	591
+Papua New Guinea	PG	PNG	598
+Paraguay	PY	PRY	600
+Peru	PE	PER	604
+Philippines	PH	PHL	608
+Pitcairn	PN	PCN	612
+Poland	PL	POL	616
+Portugal	PT	PRT	620
+Puerto Rico	PR	PRI	630
+Qatar	QA	QAT	634
+Republic of North Macedonia	MK	MKD	807
+Romania	RO	ROU	642
+Russian Federation	RU	RUS	643
+Rwanda	RW	RWA	646
+Réunion	RE	REU	638
+Saint Barthélemy	BL	BLM	652
+Saint Helena, Ascension and Tristan da Cunha	SH	SHN	654
+Saint Kitts and Nevis	KN	KNA	659
+Saint Lucia	LC	LCA	662
+Saint Martin	MF	MAF	663
+Saint Pierre and Miquelon	PM	SPM	666
+Saint Vincent and the Grenadines	VC	VCT	670
+Samoa	WS	WSM	882
+San Marino	SM	SMR	674
+Sao Tome and Principe	ST	STP	678
+Saudi Arabia	SA	SAU	682
+Senegal	SN	SEN	686
+Serbia	RS	SRB	688
+Seychelles	SC	SYC	690
+Sierra Leone	SL	SLE	694
+Singapore	SG	SGP	702
+Sint Maarten	SX	SXM	534
+Slovakia	SK	SVK	703
+Slovenia	SI	SVN	705
+Solomon Islands	SB	SLB	090
+Somalia	SO	SOM	706
+South Africa	ZA	ZAF	710
+South Georgia and the South Sandwich Islands	GS	SGS	239
+South Sudan	SS	SSD	728
+Spain	ES	ESP	724
+Sri Lanka	LK	LKA	144
+Sudan	SD	SDN	729
+Suriname	SR	SUR	740
+Svalbard and Jan Mayen	SJ	SJM	744
+Sweden	SE	SWE	752
+Switzerland	CH	CHE	756
+Syrian Arab Republic	SY	SYR	760
+Taiwan	TW	TWN	158
+Tajikistan	TJ	TJK	762
+Tanzania, United Republic of	TZ	TZA	834
+Thailand	TH	THA	764
+Timor-Leste	TL	TLS	626
+Togo	TG	TGO	768
+Tokelau	TK	TKL	772
+Tonga	TO	TON	776
+Trinidad and Tobago	TT	TTO	780
+Tunisia	TN	TUN	788
+Turkey	TR	TUR	792
+Turkmenistan	TM	TKM	795
+Turks and Caicos Islands	TC	TCA	796
+Tuvalu	TV	TUV	798
+Uganda	UG	UGA	800
+Ukraine	UA	UKR	804
+United Arab Emirates AE	ARE	784
+United Kingdom	GB	GBR	826
+United States	US	USA	840
+Uruguay	UY	URY	858
+Uzbekistan	UZ	UZB	860
+Vanuatu	VU	VUT	548
+Venezuela	VE	VEN	862
+Viet Nam	VN	VNM	704
+British Virgin Islands	VG	VGB	092
+Virgin Islands	VI	VIR	850
+Wallis and Futuna	WF	WLF	876
+Western Sahara	EH	ESH	732
+Yemen	YE	YEM	887
+Zambia	ZM	ZMB	894
+Zimbabwe	ZW	ZWE	716
+Åland Islands	AX	ALA	248
+Alabama	US	USA	840
+Alaska	US	USA	840
+Arizona	US	USA	840
+Arkansas	US	USA	840
+California	US	USA	840
+Colorado	US	USA	840
+Connecticut	US	USA	840
+Delaware	US	USA	840
+District of Columbia	US	USA	840
+Florida	US	USA	840
+Georgia	US	USA	840
+Hawaii	US	USA	840
+Idaho	US	USA	840
+Illinois	US	USA	840
+Indiana	US	USA	840
+Iowa	US	USA	840
+Kansas	US	USA	840
+Kentucky	US	USA	840
+Louisiana	US	USA	840
+Maine	US	USA	840
+Maryland	US	USA	840
+Massachusetts	US	USA	840
+Michigan	US	USA	840
+Minnesota	US	USA	840
+Mississippi	US	USA	840
+Missouri	US	USA	840
+Montana	US	USA	840
+Nebraska	US	USA	840
+Nevada	US	USA	840
+New Hampshire	US	USA	840
+New Jersey	US	USA	840
+New Mexico	US	USA	840
+New York	US	USA	840
+North Carolina	US	USA	840
+North Dakota	US	USA	840
+Ohio	US	USA	840
+Oklahoma	US	USA	840
+Oregon	US	USA	840
+Pennsylvania	US	USA	840
+Rhode Island	US	USA	840
+South Carolina	US	USA	840
+South Dakota	US	USA	840
+Tennessee	US	USA	840
+Texas	US	USA	840
+Utah	US	USA	840
+Vermont	US	USA	840
+Virginia	US	USA	840
+Washington	US	USA	840
+West Virginia	US	USA	840
+Wisconsin	US	USA	840
+Wyoming	US	USA	840`;
+
+function processTable(text,type){
+    let x = type == 'tsv' ? /\t/ : /[,](?=(?:[^\"]|\"[^\"]*\")*$)/;
+    let table = text.split(/[\r\n]+/).map(r=> r.split(x));
+    var json_arr = [];
+    table.forEach(el=> {
+        let ob = {};
+        for(var i=0; i<table[0].length; i++){
+            var key = table[0][i].toLowerCase().replace(/\W+/g,'_');
+            ob[key] = el[i];
+        }
+        json_arr.push(ob);
+    });
+    json_arr.shift();
+    return json_arr;
+}
+var counrty_data = processTable(tsv,'tsv').map(i=> {
+    return {
+        ...i,
+        ...{x:new RegExp(`\\b${i.country}\\b`,'i')}
+    }
+})
