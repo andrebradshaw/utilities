fe5f6c9b3f81e25d0e0ab1927abc9e87172c8ffc
Andre Bradshaw <bradshaw.andre@gmail.com>
2022-03-21 10:55:35 -0400

    Update dateConversions.js

--- Date Stuff/dateConversions.js
+++ Date Stuff/dateConversions.js
@@ -1,7 +1,7 @@
 function yearMonthDate(d){
   var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
   var date = new Date(d);
-  return `${date.getFullYear()}-${months[date.getMonth()]}-${date.getDate()}`;
+  return `${date.getFullYear()}-${months[date.getMonth()]}-${(date.getDate() < 10 ? '0'+date.getDate() : date.getDate() )}`;
 }
 function dateMonthYear(d){
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
