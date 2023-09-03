87e5902c1ed2f2740542a436b3a22a40e8314804
Andre Bradshaw <bradshaw.andre@gmail.com>
2023-09-03 08:02:38 -0400

    Update unqCount.js

--- data translations/unqCount.js
+++ data translations/unqCount.js
@@ -1,15 +1,17 @@
 function unqCount(records,key){
-    let keyvals = [];
-    Object.entries(records.reduce((obj, b) => {
-        obj[b] = ++obj[b] || 1;
-        return obj;
-      }, {})).forEach(kv=> {
-        let k = {};
-        k[key] = kv[0];
-        k['count'] = kv[1];
-        keyvals.push(k);
-      });
-      return keyvals.sort((a,b)=> a.count > b.count ? -1 : 0)
+    if(records?.length){
+        let keyvals = [];
+        Object.entries(records.reduce((obj, b) => {
+            obj[b] = ++obj[b] || 1;
+            return obj;
+        }, {})).forEach(kv=> {
+            let k = {};
+            k[key] = kv[0];
+            k['count'] = kv[1];
+            keyvals.push(k);
+        });
+        return keyvals.sort((a,b)=> a.count > b.count ? -1 : 0)
+    }
 }
 
 unqCount(
